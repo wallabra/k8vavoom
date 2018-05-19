52481d76e1cdb72dc21b14c0b51697fea8de6e59
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-05-20 02:46:36 +0300

    corelib: `TArray` source code reformatting

--- libs/core/array.h
+++ libs/core/array.h
@@ -29,237 +29,145 @@
 
 enum EArrayNew { E_ArrayNew };
 
-inline void* operator new(size_t, void* Ptr, EArrayNew, EArrayNew)
-{
-  return Ptr;
-}
+inline void *operator new (size_t, void *Ptr, EArrayNew, EArrayNew) { return Ptr; }
 
-template<class T> class TArray
-{
-public:
-  TArray()
-  : ArrData(0)
-  {
-    Clear();
-  }
-  TArray(ENoInit)
-  {}
-  TArray(const TArray<T>& Other)
-  : ArrData(0)
-  {
-    *this = Other;
-  }
-  ~TArray()
-  {
-    Clear();
-  }
 
-  void Clear()
-  {
-    if (ArrData)
-    {
-      for (int i = 0; i < ArrSize; i++)
-      {
-        ArrData[i].~T();
-      }
-      Z_Free(ArrData);
-    }
-    ArrData = NULL;
-    ArrNum = 0;
-    ArrSize = 0;
-  }
-  inline int Num() const { return ArrNum; }
-  inline int Length() const { return ArrNum; }
-  inline int length() const { return ArrNum; }
-  int NumAllocated() const
-  {
-    return ArrSize;
-  }
-  T* Ptr()
-  {
-    return ArrData;
-  }
-  const T* Ptr() const
-  {
-    return ArrData;
-  }
-
-  void Resize(int NewSize)
-  {
-    check(NewSize >= 0);
-
-    if (NewSize <= 0)
-    {
-      Clear();
-      return;
-    }
-
-    if (NewSize == ArrSize)
-    {
-      return;
-    }
-    T* OldData = ArrData;
-    int OldSize = ArrSize;
-    ArrSize = NewSize;
-    if (ArrNum > NewSize)
-    {
-      ArrNum = NewSize;
-    }
-
-    ArrData = (T*)Z_Malloc(ArrSize * sizeof(T));
-    for (int i = 0; i < ArrSize; i++)
-    {
-      new(&ArrData[i], E_ArrayNew, E_ArrayNew) T;
-    }
-    for (int i = 0; i < ArrNum; i++)
-    {
-      ArrData[i] = OldData[i];
-    }
-
-    if (OldData)
-    {
-      for (int i = 0; i < OldSize; i++)
-      {
-        OldData[i].~T();
-      }
-      Z_Free(OldData);
-    }
-  }
-  void SetNum(int NewNum, bool bResize = true)
-  {
-    check(NewNum >= 0);
-    if (bResize || NewNum > ArrSize)
-    {
-      Resize(NewNum);
-    }
-    ArrNum = NewNum;
-  }
-  void SetNumWithReserve(int NewNum)
-  {
-    check(NewNum >= 0);
-    if (NewNum > ArrSize)
-    {
-      Resize(NewNum + NewNum * 3 / 8 + 32);
-    }
-    ArrNum = NewNum;
-  }
-  void Condense()
-  {
-    Resize(ArrNum);
-  }
-
-  TArray<T>& operator=(const TArray<T>& Other)
-  {
-    Clear();
-
-    ArrNum = Other.ArrNum;
-    ArrSize = Other.ArrSize;
-    if (ArrSize)
-    {
-      ArrData = (T*)Z_Malloc(ArrSize * sizeof(T));
-      for (int i = 0; i < ArrSize; i++)
-      {
-        new(&ArrData[i], E_ArrayNew, E_ArrayNew) T;
-      }
-      for (int i = 0; i < ArrNum; i++)
-      {
-        ArrData[i] = Other.ArrData[i];
-      }
-    }
-    return *this;
-  }
-  T& operator[](int Index)
-  {
-    check(Index >= 0);
-    check(Index < ArrNum);
-    return ArrData[Index];
-  }
-  const T& operator[](int Index) const
-  {
-    check(Index >= 0);
-    check(Index < ArrNum);
-    return ArrData[Index];
-  }
-
-  void Insert(int Index, const T& Item)
-  {
-    if (ArrNum == ArrSize)
-    {
-      Resize(ArrSize + ArrSize * 3 / 8 + 32);
-    }
-    ArrNum++;
-    for (int i = ArrNum - 1; i > Index; i--)
-    {
-      ArrData[i] = ArrData[i - 1];
-    }
-    ArrData[Index] = Item;
-  }
-  int Append(const T& Item)
-  {
-    if (ArrNum == ArrSize)
-    {
-      Resize(ArrSize + ArrSize * 3 / 8 + 32);
-    }
-    ArrData[ArrNum] = Item;
-    ArrNum++;
-    return ArrNum - 1;
-  }
-  T& Alloc()
-  {
-    if (ArrNum == ArrSize)
-    {
-      Resize(ArrSize + ArrSize * 3 / 8 + 32);
-    }
-
-    return ArrData[ArrNum++];
-  }
-  bool RemoveIndex(int Index)
-  {
-    check(ArrData != NULL);
-    check(Index >= 0);
-    check(Index < ArrNum);
-
-    if (Index < 0 || Index >= ArrNum)
-    {
-      return false;
-    }
-
-    ArrNum--;
-    for (int i = Index; i < ArrNum; i++)
-    {
-      ArrData[i] = ArrData[i + 1];
-    }
-
-    return true;
-  }
-  void Remove(const T& Item)
-  {
-    for (int i = 0; i < ArrNum; i++)
-    {
-      if (ArrData[i] == Item)
-      {
-        RemoveIndex(i--);
-      }
-    }
-  }
-
-  friend VStream& operator << (VStream& Strm, TArray<T>& Array)
-  {
-    int NumElem = Array.Num();
-    Strm << STRM_INDEX(NumElem);
-    if (Strm.IsLoading())
-    {
-      Array.SetNum(NumElem);
-    }
-    for (int i = 0; i < Array.Num(); i++)
-    {
-      Strm << Array[i];
-    }
-    return Strm;
-  }
-
+template<class T> class TArray {
 private:
   int ArrNum;
   int ArrSize;
   T* ArrData;
+
+public:
+  TArray () : ArrNum(0), ArrSize(0), ArrData(nullptr) {}
+
+  TArray (ENoInit) {}
+
+  TArray (const TArray<T> &Other) : ArrNum(0), ArrSize(0), ArrData(nullptr) { *this = Other; }
+
+  ~TArray () { clear(); }
+
+  inline void Clear () { clear(); }
+
+  void clear () {
+    if (ArrData) {
+      for (int i = 0; i < ArrSize; ++i) ArrData[i].~T();
+      Z_Free(ArrData);
+    }
+    ArrData = nullptr;
+    ArrNum = ArrSize = 0;
+  }
+
+  inline int Num () const { return ArrNum; }
+  inline int Length () const { return ArrNum; }
+  inline int length () const { return ArrNum; }
+
+  inline int NumAllocated () const { return ArrSize; }
+
+  inline T* Ptr () { return ArrData; }
+  inline const T* Ptr () const { return ArrData; }
+
+  void Resize (int NewSize) {
+    check(NewSize >= 0);
+
+    if (NewSize <= 0) { clear(); return; }
+    if (NewSize == ArrSize) return;
+
+    T* OldData = ArrData;
+    int OldSize = ArrSize;
+    ArrSize = NewSize;
+    if (ArrNum > NewSize) ArrNum = NewSize;
+
+    ArrData = (T*)Z_Malloc(ArrSize*sizeof(T));
+    for (int i = 0; i < ArrSize; ++i) new(&ArrData[i], E_ArrayNew, E_ArrayNew)T;
+    for (int i = 0; i < ArrNum; ++i) ArrData[i] = OldData[i];
+
+    if (OldData) {
+      for (int i = 0; i < OldSize; ++i) OldData[i].~T();
+      Z_Free(OldData);
+    }
+  }
+
+  void SetNum (int NewNum, bool bResize=true) {
+    check(NewNum >= 0);
+    if (bResize || NewNum > ArrSize) Resize(NewNum);
+    ArrNum = NewNum;
+  }
+
+  void SetNumWithReserve (int NewNum) {
+    check(NewNum >= 0);
+    if (NewNum > ArrSize) Resize(NewNum+NewNum*3/8+32);
+    ArrNum = NewNum;
+  }
+
+  void Condense () {
+    Resize(ArrNum);
+  }
+
+  TArray<T> &operator = (const TArray<T> &Other) {
+    clear();
+    ArrNum = Other.ArrNum;
+    ArrSize = Other.ArrSize;
+    if (ArrSize) {
+      ArrData = (T*)Z_Malloc(ArrSize*sizeof(T));
+      for (int i = 0; i < ArrSize; ++i) new(&ArrData[i], E_ArrayNew, E_ArrayNew)T;
+      for (int i = 0; i < ArrNum; ++i) ArrData[i] = Other.ArrData[i];
+    }
+    return *this;
+  }
+
+  inline T &operator [] (int Index) {
+    check(Index >= 0);
+    check(Index < ArrNum);
+    return ArrData[Index];
+  }
+
+  inline const T &operator [] (int Index) const {
+    check(Index >= 0);
+    check(Index < ArrNum);
+    return ArrData[Index];
+  }
+
+  void Insert (int Index, const T &Item) {
+    if (ArrNum == ArrSize) Resize(ArrSize+ArrSize*3/8+32);
+    ++ArrNum;
+    for (int i = ArrNum-1; i > Index; --i) ArrData[i] = ArrData[i-1];
+    ArrData[Index] = Item;
+  }
+
+  inline int Append (const T &Item) {
+    if (ArrNum == ArrSize) Resize(ArrSize+ArrSize*3/8+32);
+    ArrData[ArrNum] = Item;
+    ++ArrNum;
+    return ArrNum-1;
+  }
+
+  inline T &Alloc () {
+    if (ArrNum == ArrSize) Resize(ArrSize+ArrSize*3/8+32);
+    return ArrData[ArrNum++];
+  }
+
+  bool RemoveIndex (int Index) {
+    check(ArrData != nullptr);
+    check(Index >= 0);
+    check(Index < ArrNum);
+    if (Index < 0 || Index >= ArrNum) return false;
+    --ArrNum;
+    for (int i = Index; i < ArrNum; ++i) ArrData[i] = ArrData[i+1];
+    return true;
+  }
+
+  void Remove (const T &Item) {
+    for (int i = 0; i < ArrNum; ++i) {
+      if (ArrData[i] == Item) RemoveIndex(i--);
+    }
+  }
+
+  friend VStream &operator << (VStream &Strm, TArray<T> &Array) {
+    int NumElem = Array.Num();
+    Strm << STRM_INDEX(NumElem);
+    if (Strm.IsLoading()) Array.SetNum(NumElem);
+    for (int i = 0; i < Array.Num(); ++i) Strm << Array[i];
+    return Strm;
+  }
 };
