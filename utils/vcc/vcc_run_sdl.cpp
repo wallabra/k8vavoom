ac4d6e1c75f21215c19e8a31a692b4eb583ef5e1
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-05-25 11:00:40 +0300

    vccrun: new class: VideoMode

---
+++ utils/vcc/vcc_run_sdl.cpp
@@ -0,0 +1,68 @@
+//**************************************************************************
+//**
+//**  ##   ##    ##    ##   ##   ####     ####   ###     ###
+//**  ##   ##  ##  ##  ##   ##  ##  ##   ##  ##  ####   ####
+//**   ## ##  ##    ##  ## ##  ##    ## ##    ## ## ## ## ##
+//**   ## ##  ########  ## ##  ##    ## ##    ## ##  ###  ##
+//**    ###   ##    ##   ###    ##  ##   ##  ##  ##       ##
+//**     #    ##    ##    #      ####     ####   ##       ##
+//**
+//**  $Id$
+//**
+//**  Copyright (C) 1999-2006 Jānis Legzdiņš
+//**  Copyright (C) 2018 Ketmar Dark
+//**
+//**  This program is free software; you can redistribute it and/or
+//**  modify it under the terms of the GNU General Public License
+//**  as published by the Free Software Foundation; either version 2
+//**  of the License, or (at your option) any later version.
+//**
+//**  This program is distributed in the hope that it will be useful,
+//**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//**  GNU General Public License for more details.
+//**
+//**************************************************************************
+
+#include "vcc_run_sdl.h"
+
+
+bool VVideoMode::canInit () {
+#ifdef VCCRUN_HAS_SDL
+  return true;
+#else
+  return false;
+#endif
+}
+
+
+bool VVideoMode::hasOpenGL () {
+#if defined(VCCRUN_HAS_SDL) && defined(VCCRUN_HAS_OPENGL)
+  return true;
+#else
+  return false;
+#endif
+}
+
+
+bool VVideoMode::isInitialized () {
+  return false;
+}
+
+
+int VVideoMode::getWidth () {
+  return 0;
+}
+
+
+int VVideoMode::getHeight () {
+  return 0;
+}
+
+
+// ////////////////////////////////////////////////////////////////////////// //
+IMPLEMENT_FUNCTION(VVideoMode, canInit) { RET_BOOL(VVideoMode::canInit()); }
+IMPLEMENT_FUNCTION(VVideoMode, hasOpenGL) { RET_BOOL(VVideoMode::hasOpenGL()); }
+IMPLEMENT_FUNCTION(VVideoMode, isInitialized) { RET_BOOL(VVideoMode::isInitialized()); }
+IMPLEMENT_FUNCTION(VVideoMode, getWidth) { RET_INT(VVideoMode::getWidth()); }
+IMPLEMENT_FUNCTION(VVideoMode, getHeight) { RET_INT(VVideoMode::getHeight()); }
---
+++ utils/vcc/vcc_run_sdl.h
@@ -0,0 +1,59 @@
+//**************************************************************************
+//**
+//**  ##   ##    ##    ##   ##   ####     ####   ###     ###
+//**  ##   ##  ##  ##  ##   ##  ##  ##   ##  ##  ####   ####
+//**   ## ##  ##    ##  ## ##  ##    ## ##    ## ## ## ## ##
+//**   ## ##  ########  ## ##  ##    ## ##    ## ##  ###  ##
+//**    ###   ##    ##   ###    ##  ##   ##  ##  ##       ##
+//**     #    ##    ##    #      ####     ####   ##       ##
+//**
+//**  $Id$
+//**
+//**  Copyright (C) 1999-2006 Jānis Legzdiņš
+//**  Copyright (C) 2018 Ketmar Dark
+//**
+//**  This program is free software; you can redistribute it and/or
+//**  modify it under the terms of the GNU General Public License
+//**  as published by the Free Software Foundation; either version 2
+//**  of the License, or (at your option) any later version.
+//**
+//**  This program is distributed in the hope that it will be useful,
+//**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//**  GNU General Public License for more details.
+//**
+//**************************************************************************
+#ifndef VCCRUN_SDL_HEADER_FILE
+#define VCCRUN_SDL_HEADER_FILE
+
+#include "vcc_run.h"
+
+#ifdef VCCRUN_HAS_SDL
+
+class VVideoMode : public VObject {
+  DECLARE_CLASS(VVideoMode, VObject, 0)
+  NO_DEFAULT_CONSTRUCTOR(VLevel)
+
+#ifdef VCCRUN_HAS_SDL
+#endif
+#ifdef VCCRUN_HAS_OPENGL
+#endif
+
+  static bool canInit ();
+  static bool hasOpenGL ();
+  static bool isInitialized ();
+  static int getWidth ();
+  static int getHeight ();
+
+  // static
+  DECLARE_FUNCTION(canInit)
+  DECLARE_FUNCTION(hasOpenGL)
+  DECLARE_FUNCTION(isInitialized)
+  DECLARE_FUNCTION(getWidth)
+  DECLARE_FUNCTION(getHeight)
+};
+
+
+#endif
+
+#endif
