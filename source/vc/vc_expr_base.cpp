e58b278ea7482e14a723b305fc12f06eaf1b3115
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-06-01 21:57:17 +0300

    vc: cosmetix

--- source/vc/vc_expr_base.cpp
+++ source/vc/vc_expr_base.cpp
@@ -279,7 +279,7 @@ bool VExpression::IsNameConst () const { return false; }
 vint32 VExpression::GetIntConst () const { ParseError(Loc, "Integer constant expected"); return 0; }
 float VExpression::GetFloatConst () const { ParseError(Loc, "Float constant expected"); return 0.0; }
 VStr VExpression::GetStrConst (VPackage *) const { ParseError(Loc, "String constant expected"); return VStr(); }
-VName VExpression::GetNameConst () const { ParseError(Loc, "Name constant expected"); return NAME_none; }
+VName VExpression::GetNameConst () const { ParseError(Loc, "Name constant expected"); return NAME_None; }
 bool VExpression::IsDefaultObject () const { return false; }
 bool VExpression::IsPropertyAssign () const { return false; }
 bool VExpression::IsDynArraySetNum () const { return false; }
