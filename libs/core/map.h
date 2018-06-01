03b0f72685f9228f8ea8877bfc22918efb15b8b4
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-06-01 09:52:30 +0300

    cirelib: map cosmetix

--- libs/core/map.h
+++ libs/core/map.h
@@ -27,85 +27,67 @@
 //**
 //**************************************************************************
 
-template<class TK, class TV> class TMap
-{
+template<class TK, class TV> class TMap {
 protected:
-  struct TPair
-  {
-    TK      Key;
-    TV      Value;
-    vint32    HashNext;
+  struct TPair {
+    TK Key;
+    TV Value;
+    vint32 HashNext;
   };
 
   TArray<TPair> Pairs;
-  vint32*     HashTable;
-  vint32      HashSize;
+  vint32 *HashTable;
+  vint32 HashSize;
 
-  void Rehash()
-  {
+  void Rehash () {
     guardSlow(TMap::Rehash);
     checkSlow(HashSize >= 16);
-    if (HashTable)
-    {
+    if (HashTable) {
       delete[] HashTable;
-      HashTable = NULL;
+      HashTable = nullptr;
     }
     HashTable = new vint32[HashSize];
-    for (int i = 0; i < HashSize; i++)
-    {
-      HashTable[i] = -1;
-    }
-    for (int i = 0; i < Pairs.Num(); i++)
-    {
-      int Hash = GetTypeHash(Pairs[i].Key) & (HashSize - 1);
+    for (int i = 0; i < HashSize; ++i) HashTable[i] = -1;
+    for (int i = 0; i < Pairs.Num(); ++i) {
+      int Hash = GetTypeHash(Pairs[i].Key)&(HashSize-1);
       Pairs[i].HashNext = HashTable[Hash];
       HashTable[Hash] = i;
     }
     unguardSlow;
   }
-  void Relax()
-  {
+
+  void Relax () {
     guardSlow(TMap::Relax);
-    while (HashSize > Pairs.Num() + 16)
-    {
-      HashSize >>= 1;
-    }
+    while (HashSize > Pairs.Num()+16) HashSize >>= 1;
     Rehash();
     unguardSlow;
   }
 
 public:
-  TMap()
-  : HashTable(NULL)
-  , HashSize(16)
-  {
+  TMap () : HashTable(nullptr), HashSize(16) {
     guardSlow(TMap::TMap);
     Rehash();
     unguardSlow;
   }
-  TMap(TMap& Other)
-  : Pairs(Other.Pairs)
-  , HashTable(NULL)
-  , HashSize(Other.HashSize)
-  {
+
+  TMap (TMap &Other) : Pairs(Other.Pairs), HashTable(nullptr), HashSize(Other.HashSize) {
     guardSlow(TMap::TMap);
     Rehash();
     unguardSlow;
   }
-  ~TMap()
-  {
+
+  ~TMap () {
     guardSlow(TMap::~TMap);
-    if (HashTable)
-    {
+    if (HashTable) {
       delete[] HashTable;
-      HashTable = NULL;
+      HashTable = nullptr;
     }
-    HashTable = NULL;
+    HashTable = nullptr;
     HashSize = 0;
     unguardSlow;
   }
-  TMap& operator = (const TMap& Other)
-  {
+
+  TMap &operator = (const TMap &Other) {
     guardSlow(TMap::operator=);
     Pairs = Other.Pairs;
     HashSize = Other.HashSize;
@@ -113,126 +95,88 @@ public:
     return *this;
     unguardSlow;
   }
-  void Set(TK Key, const TV& Value)
-  {
+
+  void Set (const TK &Key, const TV &Value) {
     guardSlow(TMap::Set);
     int HashIndex = GetTypeHash(Key) & (HashSize - 1);
-    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext)
-    {
-      if (Pairs[i].Key == Key)
-      {
+    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext) {
+      if (Pairs[i].Key == Key) {
         Pairs[i].Value = Value;
         return;
       }
     }
-    TPair& Pair = Pairs.Alloc();
+    TPair &Pair = Pairs.Alloc();
     Pair.HashNext = HashTable[HashIndex];
-    HashTable[HashIndex] = Pairs.Num() - 1;
+    HashTable[HashIndex] = Pairs.Num()-1;
     Pair.Key = Key;
     Pair.Value = Value;
-    if (HashSize * 2 + 16 < Pairs.Num())
-    {
+    if (HashSize*2+16 < Pairs.Num()) {
       HashSize <<= 1;
       Rehash();
     }
     unguardSlow;
   }
-  bool Remove(TK Key)
-  {
+
+  //FIXME: this is dog slow!
+  bool Remove (const TK &Key) {
     guardSlow(TMap::Remove);
     bool Removed = false;
-    int HashIndex = GetTypeHash(Key) & (HashSize - 1);
-    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext)
-    {
-      if (Pairs[i].Key == Key)
-      {
+    int HashIndex = GetTypeHash(Key)&(HashSize-1);
+    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext) {
+      if (Pairs[i].Key == Key) {
         Pairs.RemoveIndex(i);
         Removed = true;
         break;
       }
     }
-    if (Removed)
-    {
-      Relax();
-    }
+    if (Removed) Relax();
     return Removed;
     unguardSlow;
   }
-  TV* Find(TK Key)
-  {
+
+  //WARNING! returned pointer will be invalidated by any map mutation
+  TV *Find (const TK &Key) {
     guardSlow(TMap::Find);
-    int HashIndex = GetTypeHash(Key) & (HashSize - 1);
-    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext)
-    {
-      if (Pairs[i].Key == Key)
-      {
-        return &Pairs[i].Value;
-      }
+    int HashIndex = GetTypeHash(Key)&(HashSize-1);
+    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext) {
+      if (Pairs[i].Key == Key) return &Pairs[i].Value;
     }
-    return NULL;
-    unguardSlow;
-  }
-  const TV* Find(TK Key) const
-  {
-    guardSlow(TMap::Find);
-    int HashIndex = GetTypeHash(Key) & (HashSize - 1);
-    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext)
-    {
-      if (Pairs[i].Key == Key)
-      {
-        return &Pairs[i].Value;
-      }
-    }
-    return NULL;
-    unguardSlow;
-  }
-  const TV FindPtr(TK Key) const
-  {
-    guardSlow(TMap::Find);
-    int HashIndex = GetTypeHash(Key) & (HashSize - 1);
-    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext)
-    {
-      if (Pairs[i].Key == Key)
-      {
-        return Pairs[i].Value;
-      }
-    }
-    return NULL;
+    return nullptr;
     unguardSlow;
   }
 
-  class TIterator
-  {
+  //WARNING! returned pointer will be invalidated by any map mutation
+  const TV *Find (const TK &Key) const {
+    guardSlow(TMap::Find);
+    int HashIndex = GetTypeHash(Key)&(HashSize-1);
+    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext) {
+      if (Pairs[i].Key == Key) return &Pairs[i].Value;
+    }
+    return nullptr;
+    unguardSlow;
+  }
+
+  const TV FindPtr (const TK &Key) const {
+    guardSlow(TMap::Find);
+    int HashIndex = GetTypeHash(Key)&(HashSize-1);
+    for (int i = HashTable[HashIndex]; i != -1; i = Pairs[i].HashNext) {
+      if (Pairs[i].Key == Key) return Pairs[i].Value;
+    }
+    return nullptr;
+    unguardSlow;
+  }
+
+  class TIterator {
   private:
-    TMap& Map;
+    TMap &Map;
     vint32 Index;
   public:
-    TIterator(TMap& AMap)
-    : Map(AMap)
-    , Index(0)
-    {}
-    operator bool() const
-    {
-      return Index < Map.Pairs.Num();
-    }
-    void operator ++ ()
-    {
-      Index++;
-    }
-    const TK& GetKey() const
-    {
-      return Map.Pairs[Index].Key;
-    }
-    const TV& GetValue() const
-    {
-      return Map.Pairs[Index].Value;
-    }
-    void RemoveCurrent()
-    {
-      Map.Pairs.RemoveIndex(Index);
-      Map.Relax();
-      Index--;
-    }
+    TIterator (TMap &AMap) : Map(AMap), Index(0) {}
+    inline operator bool () const { return Index < Map.Pairs.Num(); }
+    inline void operator ++ () { ++Index; }
+    inline const TK &GetKey () const { return Map.Pairs[Index].Key; }
+    inline const TV &GetValue () const { return Map.Pairs[Index].Value; }
+    inline void RemoveCurrent () { Map.Pairs.RemoveIndex(Index); Map.Relax(); --Index; }
   };
 
   friend class TIterator;
