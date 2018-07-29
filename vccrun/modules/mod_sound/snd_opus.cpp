98cf75b7b02e4acc0c6f3d35105f797ef4dce3f6
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-07-29 07:34:11 +0300

    vccrun: added opus decoder

--- vccrun/modules/mod_sound/snd_opus.cpp
+++ vccrun/modules/mod_sound/snd_opus.cpp
@@ -23,33 +23,27 @@
 //**
 //**************************************************************************
 
-#include <vorbis/codec.h>
+#include <opus/opusfile.h>
 
 #include "sound.h"
 
 
-class VVorbisAudioCodec : public VAudioCodec {
+class VOpusAudioCodec : public VAudioCodec {
 public:
   int InitLevel;
   VStream *Strm;
   bool FreeStream;
   int BytesLeft;
-
-  ogg_sync_state oy;
-  ogg_stream_state os;
-  vorbis_info vi;
-  vorbis_comment vc;
-  vorbis_dsp_state vd;
-  vorbis_block vb;
-
   bool eos;
 
-  VVorbisAudioCodec (VStream *, bool);
-  virtual ~VVorbisAudioCodec () override;
+  OggOpusFile *opus; // nullptr means "open failed"
+  OpusHead head;
+
+  VOpusAudioCodec (VStream *, bool);
+  virtual ~VOpusAudioCodec () override;
   bool Init ();
   void Cleanup ();
   virtual int Decode (short *, int) override;
-  int ReadData ();
   virtual bool Finished () override;
   virtual void Restart () override;
 
@@ -57,14 +51,61 @@ public:
 };
 
 
-class VVorbisSampleLoader : public VSampleLoader {
+class VOpusSampleLoader : public VSampleLoader {
 public:
   virtual void Load (sfxinfo_t &, VStream &) override;
 };
 
-IMPLEMENT_AUDIO_CODEC(VVorbisAudioCodec, "Vorbis");
+IMPLEMENT_AUDIO_CODEC(VOpusAudioCodec, "Opus");
 
-VVorbisSampleLoader VorbisSampleLoader;
+VOpusSampleLoader OpusSampleLoader;
+
+
+// ////////////////////////////////////////////////////////////////////////// //
+static int opusStreamReadFn (void *astrm, unsigned char *_ptr, int _nbytes) {
+  VStream *strm = (VStream *)astrm;
+  if (strm->IsError()) return -1;
+  auto left = strm->TotalSize()-strm->Tell();
+  if (left < 1) return 0;
+  if (_nbytes > left) _nbytes = left;
+  strm->Serialize(_ptr, _nbytes);
+  return (strm->IsError() ? -1 : _nbytes);
+}
+
+
+static int opusStreamSeekFn (void *astrm, opus_int64 _offset, int _whence) {
+  VStream *strm = (VStream *)astrm;
+  if (strm->IsError()) return -1;
+  switch (_whence) {
+    case 0: // SEEK_SET
+      break;
+    case 1: // SEEK_CUR
+      _offset += strm->Tell();
+      break;
+    case 2: // SEEK_END
+      _offset += strm->TotalSize();
+      break;
+    default: return -1;
+  }
+  if (_offset < 0) _offset = 0;
+  if (_offset > strm->TotalSize()) _offset = strm->TotalSize();
+  strm->Seek((int)_offset);
+  return (strm->IsError() ? -1 : 0);
+}
+
+
+static opus_int64 opusStreamTellFn (void *astrm) {
+  VStream *strm = (VStream *)astrm;
+  return (strm->IsError() ? 0 : strm->Tell());
+}
+
+
+static const OpusFileCallbacks opusStreamCB = {
+  .read = &opusStreamReadFn,
+  .seek = &opusStreamSeekFn,
+  .tell = &opusStreamTellFn,
+  .close = nullptr,
+};
 
 
 //==========================================================================
@@ -72,13 +113,15 @@ VVorbisSampleLoader VorbisSampleLoader;
 //  VVorbisAudioCodec::VVorbisAudioCodec
 //
 //==========================================================================
-VVorbisAudioCodec::VVorbisAudioCodec (VStream *AStrm, bool AFreeStream)
+VOpusAudioCodec::VOpusAudioCodec (VStream *AStrm, bool AFreeStream)
   : Strm(AStrm)
   , FreeStream(AFreeStream)
+  , BytesLeft(0)
+  , eos(false)
+  , opus(nullptr)
 {
   BytesLeft = Strm->TotalSize();
   Strm->Seek(0);
-  ogg_sync_init(&oy);
 }
 
 
@@ -87,7 +130,8 @@ VVorbisAudioCodec::VVorbisAudioCodec (VStream *AStrm, bool AFreeStream)
 //  VVorbisAudioCodec::~VVorbisAudioCodec
 //
 //==========================================================================
-VVorbisAudioCodec::~VVorbisAudioCodec () {
+VOpusAudioCodec::~VOpusAudioCodec () {
+  if (opus) { op_free(opus); opus = nullptr; }
   if (InitLevel > 0) {
     Cleanup();
     if (FreeStream) {
@@ -96,7 +140,6 @@ VVorbisAudioCodec::~VVorbisAudioCodec () {
     }
     Strm = nullptr;
   }
-  ogg_sync_clear(&oy);
 }
 
 
@@ -105,49 +148,31 @@ VVorbisAudioCodec::~VVorbisAudioCodec () {
 //  VVorbisAudioCodec::Init
 //
 //==========================================================================
-bool VVorbisAudioCodec::Init () {
-  ogg_page og;
-  ogg_packet op;
-
-  eos = false;
+bool VOpusAudioCodec::Init () {
+  //if (eos || BytesLeft == 0) return false;
   InitLevel = 0;
 
-  // read some data
-  ReadData();
+  //opus = op_test_callbacks(Strm, &opusStreamCB, nullptr, 0, nullptr);
+  vuint8 buf[512];
+  if (BytesLeft < 57 || Strm->IsError()) { BytesLeft = 0; eos = true; return false; }
+  int tord = (BytesLeft < 512 ? BytesLeft : 512);
+  Strm->Serialize(buf, tord);
+  if (Strm->IsError()) { BytesLeft = 0; eos = true; return false; }
+  if (op_test(&head, buf, tord) != 0) { BytesLeft = 0; eos = true; return false; }
 
-  // get the first page
-  if (ogg_sync_pageout(&oy, &og) != 1) return false; // not a Vorbis file
+  if (head.channel_count < 1 || head.channel_count > 2) return false;
+  //fprintf(stderr, "channels: %d\n", head.channel_count);
 
-  ogg_stream_init(&os, ogg_page_serialno(&og));
-  vorbis_info_init(&vi);
-  vorbis_comment_init(&vc);
+  Strm->Seek(0);
+  if (Strm->IsError()) { BytesLeft = 0; eos = true; return false; }
+  opus = op_open_callbacks(Strm, &opusStreamCB, nullptr, 0, nullptr);
+  if (!opus) { BytesLeft = 0; eos = true; return false; }
+
+  SampleRate = 48000; // always
+  SampleBits = 16;
+  NumChannels = 2; //head.channel_count;
   InitLevel = 1;
 
-  if (ogg_stream_pagein(&os, &og) < 0) return false; // stream version mismatch perhaps
-  if (ogg_stream_packetout(&os, &op) != 1) return false; // no page? must not be vorbis
-  if (vorbis_synthesis_headerin(&vi, &vc, &op) < 0) return false; // not a vorbis header
-
-  // we need 2 more headers
-  int i = 0;
-  while (i < 2) {
-    int result = ogg_stream_packetout(&os, &op);
-    if (result < 0) return false; // corrupt header
-    if (result > 0) {
-      if (vorbis_synthesis_headerin(&vi, &vc, &op)) return false; // corrupt header
-      ++i;
-    } else if (ogg_sync_pageout(&oy, &og) > 0) {
-      ogg_stream_pagein(&os, &og);
-    } else if (ReadData() == 0 && i < 2) {
-      // out of data while reading headers
-      return false;
-    }
-  }
-
-  // parsed all three headers; initialise the Vorbis decoder
-  vorbis_synthesis_init(&vd, &vi);
-  vorbis_block_init(&vd, &vb);
-  SampleRate = vi.rate;
-  InitLevel = 2;
   return true;
 }
 
@@ -157,10 +182,8 @@ bool VVorbisAudioCodec::Init () {
 //  VVorbisAudioCodec::Cleanup
 //
 //==========================================================================
-void VVorbisAudioCodec::Cleanup () {
-  if (InitLevel > 0) ogg_stream_clear(&os);
-  if (InitLevel > 1) { vorbis_block_clear(&vb); vorbis_dsp_clear(&vd); }
-  if (InitLevel > 0) { vorbis_comment_clear(&vc); vorbis_info_clear(&vi); }
+void VOpusAudioCodec::Cleanup () {
+  if (opus) { op_free(opus); opus = nullptr; }
   InitLevel = 0;
 }
 
@@ -169,63 +192,21 @@ void VVorbisAudioCodec::Cleanup () {
 //
 //  VVorbisAudioCodec::Decode
 //
+//  `NumSamples` is number of frames, actually
+//
 //==========================================================================
-int VVorbisAudioCodec::Decode (short *Data, int NumSamples) {
-  ogg_page og;
-  ogg_packet op;
+int VOpusAudioCodec::Decode (short *Data, int NumSamples) {
+  if (!opus) return 0;
   int CurSample = 0;
-
-  while (!eos) {
-    // while we have data ready, read it
-    float **pcm;
-    int samples = vorbis_synthesis_pcmout(&vd, &pcm);
-    if (samples > 0) {
-      int bout = (CurSample+samples < NumSamples ? samples : NumSamples-CurSample);
-      for (int i = 0; i < 2; ++i) {
-        short *ptr = Data+CurSample*2+(vi.channels > 1 ? i : 0);
-        float *mono = pcm[vi.channels > 1 ? i : 0];
-        for (int j = 0; j < bout; ++j) {
-          int val = int(mono[j]*32767.f);
-          // clipping
-               if (val > 32767) val = 32767;
-          else if (val < -32768) val = -32768;
-          *ptr = val;
-          ptr += 2;
-        }
-      }
-      // tell libvorbis how many samples we actually consumed
-      vorbis_synthesis_read(&vd, bout);
-      CurSample += bout;
-      if (CurSample == NumSamples) return CurSample;
-    }
-
-    if (ogg_stream_packetout(&os, &op) > 0) {
-      // we have a packet: decode it
-      if (vorbis_synthesis(&vb, &op) == 0) vorbis_synthesis_blockin(&vd, &vb);
-    } else if (ogg_sync_pageout(&oy, &og) > 0) {
-      ogg_stream_pagein(&os, &og);
-    } else if (ReadData() == 0) {
-      eos = true;
-    }
+  while (!eos && CurSample < NumSamples) {
+    int toread = (NumSamples-CurSample);
+    auto rdsmp = op_read_stereo(opus, Data+CurSample*2, toread*2); // we always has two channels, and a room for this
+    if (rdsmp == 0) { eos = true; break; }
+    //fprintf(stderr, "toread: %d; rdsmp: %d\n", toread, rdsmp);
+    CurSample += rdsmp;
   }
+  if (!eos) eos = (Strm->IsError() || Strm->Tell() >= BytesLeft);
   return CurSample;
-}
-
-
-//==========================================================================
-//
-//  VVorbisAudioCodec::ReadData
-//
-//==========================================================================
-int VVorbisAudioCodec::ReadData () {
-  if (!BytesLeft) return 0;
-  char *buffer = ogg_sync_buffer(&oy, 4096);
-  int bytes = 4096;
-  if (bytes > BytesLeft) bytes = BytesLeft;
-  Strm->Serialise(buffer, bytes);
-  ogg_sync_wrote(&oy, bytes);
-  BytesLeft -= bytes;
-  return bytes;
 }
 
 
@@ -234,7 +215,7 @@ int VVorbisAudioCodec::ReadData () {
 //  VVorbisAudioCodec::Finished
 //
 //==========================================================================
-bool VVorbisAudioCodec::Finished () {
+bool VOpusAudioCodec::Finished () {
   return eos;
 }
 
@@ -244,7 +225,7 @@ bool VVorbisAudioCodec::Finished () {
 //  VVorbisAudioCodec::Restart
 //
 //==========================================================================
-void VVorbisAudioCodec::Restart () {
+void VOpusAudioCodec::Restart () {
   Cleanup();
   Strm->Seek(0);
   BytesLeft = Strm->TotalSize();
@@ -257,8 +238,8 @@ void VVorbisAudioCodec::Restart () {
 //  VVorbisAudioCodec::Create
 //
 //==========================================================================
-VAudioCodec *VVorbisAudioCodec::Create (VStream *InStrm) {
-  VVorbisAudioCodec *Codec = new VVorbisAudioCodec(InStrm, true);
+VAudioCodec *VOpusAudioCodec::Create (VStream *InStrm) {
+  VOpusAudioCodec *Codec = new VOpusAudioCodec(InStrm, true);
   if (!Codec->Init()) {
     Codec->Cleanup();
     delete Codec;
@@ -274,8 +255,8 @@ VAudioCodec *VVorbisAudioCodec::Create (VStream *InStrm) {
 //  VVorbisAudioCodec::Create
 //
 //==========================================================================
-void VVorbisSampleLoader::Load (sfxinfo_t &Sfx, VStream &Stream) {
-  VVorbisAudioCodec *Codec = new VVorbisAudioCodec(&Stream, false);
+void VOpusSampleLoader::Load (sfxinfo_t &Sfx, VStream &Stream) {
+  VOpusAudioCodec *Codec = new VOpusAudioCodec(&Stream, false);
   if (!Codec->Init()) {
     Codec->Cleanup();
     delete Codec;
@@ -290,7 +271,6 @@ void VVorbisSampleLoader::Load (sfxinfo_t &Sfx, VStream &Stream) {
     if (SamplesDecoded > 0) {
       int OldPos = Data.length();
       Data.SetNumWithReserve(Data.length()+SamplesDecoded);
-      //for (int i = 0; i < SamplesDecoded; ++i) Data[OldPos+i] = Buf[i*2];
       for (int i = 0; i < SamplesDecoded; ++i) {
         // mix it
         int v = Buf[i*2]+Buf[i*2+1];
