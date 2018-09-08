f60837d1950e3547afb7cdedce94b653c1a38f68
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-09-08 19:44:17 +0300

    engine: shader cleanup

--- basev/common/glshaders/surf_model.fs
+++ basev/common/glshaders/surf_model.fs
@@ -15,90 +15,53 @@ varying vec3 VertToView;
 varying vec3 VPos;
 varying vec2 TextureCoordinate;
 
-void main ()
-{
+void main () {
   vec4 FinalColour;
   vec4 TexColour;
   float DistVPos;
 
-  DistVPos = sqrt(dot (VPos, VPos));
+  DistVPos = /*sqrt*/(dot(VPos, VPos));
 
-  if ((DistVPos < 0.0))
-  {
-    discard;
-  };
-  float DistToView;
+  if (DistVPos < 0.0) discard;
 
-  DistToView = sqrt(dot (VertToView, VertToView));
+  float DistToView = /*sqrt*/(dot(VertToView, VertToView));
 
-  if ((DistToView < 0.0))
-  {
-    discard;
-  };
-  TexColour = (texture2D (Texture, TextureCoordinate) * Light);
+  if (DistToView < 0.0) discard;
+
+  TexColour = texture2D(Texture, TextureCoordinate)*Light;
+
+  if (TexColour.w < 0.1) discard;
+
   FinalColour.xyz = TexColour.xyz;
   FinalColour.w = TexColour.w;
 
-  if ((TexColour.w < 0.1))
-  {
-    discard;
-  };
+  if (FogEnabled) {
+    float z = (gl_FragCoord.z/gl_FragCoord.w);
 
-  if (FogEnabled)
-  {
-    float z;
     float FogFactor;
 
-    z = (gl_FragCoord.z / gl_FragCoord.w);
-
-    if ((FogType == 3))
-    {
-      FogFactor = exp2(((
-      ((-(FogDensity) * FogDensity) * z)
-      * z) * 1.442695));
+    if (FogType == 3) {
+      FogFactor = exp2((((-FogDensity*FogDensity)*z)*z)*1.442695);
+    } else if (FogType == 2) {
+      FogFactor = exp2((-FogDensity*z)*1.442695);
+    } else {
+      FogFactor = (FogEnd-z)/(FogEnd-FogStart);
     }
-    else
-    {
-      if ((FogType == 2))
-      {
-        FogFactor = exp2(((
-          -(FogDensity)
-         * z) * 1.442695));
-      }
-      else
-      {
-        FogFactor = ((FogEnd - z) / (FogEnd - FogStart));
-      };
-    };
-    float ClampFogFactor;
 
-    ClampFogFactor = clamp (FogFactor, 0.0, 1.0);
+    float ClampFogFactor = clamp(FogFactor, 0.0, 1.0);
     FogFactor = ClampFogFactor;
 
-    float ClampTransp;
+    float ClampTransp = clamp (((ClampFogFactor - 0.1) / 0.9), 0.0, 1.0);
 
-    ClampTransp = clamp (((ClampFogFactor - 0.1) / 0.9), 0.0, 1.0);
-    FinalColour.xyz = mix (FogColour.xyz, TexColour.xyz, (ClampTransp * (ClampTransp *
-      (3.0 - (2.0 * ClampTransp))
-    )));
-    FinalColour.w = mix (FogColour.w, TexColour.w, (ClampTransp * (ClampTransp *
-      (3.0 - (2.0 * ClampTransp))
-    )));
-  };
-
-  if ((AllowTransparency == bool(0)))
-  {
-    if (((InAlpha == 1.0) && (FinalColour.w < 0.666)))
-    {
-      discard;
-    };
+    FinalColour.xyz = mix(FogColour.xyz, TexColour.xyz, ClampTransp*(ClampTransp*(3.0-(2.0*ClampTransp))));
+    FinalColour.w = mix(FogColour.w, TexColour.w, ClampTransp*(ClampTransp*(3.0-(2.0*ClampTransp))));
   }
-  else
-  {
-    if ((FinalColour.w < 0.1))
-    {
-      discard;
-    };
-  };
+
+  if (!AllowTransparency) {
+    if (InAlpha == 1.0 && FinalColour.w < 0.666) discard;
+  } else {
+    if (FinalColour.w < 0.1) discard;
+  }
+
   gl_FragColor = FinalColour;
 }
