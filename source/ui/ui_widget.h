65630d3e2460701e3a263497551d1317d28abba5
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-09-24 16:56:06 +0300

    engine: reformatted some ui widget code

--- source/ui/ui_widget.h
+++ source/ui/ui_widget.h
@@ -23,348 +23,269 @@
 //**
 //**************************************************************************
 
-struct VClipRect
-{
-  float   OriginX;  //  Origin of the widget, in absolute coordinates.
-  float   OriginY;
+struct VClipRect {
+  // origin of the widget, in absolute coordinates
+  float OriginX;
+  float OriginY;
 
-  float   ScaleX;   //  Accomulative scale.
-  float   ScaleY;
+  // accumulative scale
+  float ScaleX;
+  float ScaleY;
 
-  float   ClipX1;   //  Clipping rectangle, in absolute coordinates.
-  float   ClipY1;
-  float   ClipX2;
-  float   ClipY2;
+  // clipping rectangle, in absolute coordinates
+  float ClipX1;
+  float ClipY1;
+  float ClipX2;
+  float ClipY2;
 
-  bool HasArea() const
-  {
-    return ClipX1 < ClipX2 && ClipY1 < ClipY2;
-  }
+  inline bool HasArea () const { return (ClipX1 < ClipX2 && ClipY1 < ClipY2); }
 };
 
-class VWidget : public VObject
-{
+
+class VWidget : public VObject {
   DECLARE_CLASS(VWidget, VObject, 0)
   NO_DEFAULT_CONSTRUCTOR(VWidget)
 
 private:
-  //  Parent container widget.
+  // parent container widget
   VWidget *ParentWidget;
-  //  Linked list of child widgets.
+  // linked list of child widgets
   VWidget *FirstChildWidget;
   VWidget *LastChildWidget;
-  //  Links in the linked list of widgets.
+  // links in the linked list of widgets
   VWidget *PrevWidget;
   VWidget *NextWidget;
 
-  //  Position of the widget in the parent widget.
-  int         PosX;
-  int         PosY;
-  //  Offset for children.
-  int         OfsX;
-  int         OfsY;
-  //  Size of the child area of the widget.
-  int         SizeWidth;
-  int         SizeHeight;
-  //  Scaling of the widget.
-  float       SizeScaleX;
-  float       SizeScaleY;
+  // position of the widget in the parent widget
+  int PosX;
+  int PosY;
+  // offset for children
+  int OfsX;
+  int OfsY;
+  // size of the child area of the widget
+  int SizeWidth;
+  int SizeHeight;
+  // scaling of the widget
+  float SizeScaleX;
+  float SizeScaleY;
 
-  VClipRect     ClipRect;
+  VClipRect ClipRect;
 
-  //  Currently focused child widget.
+  // currently focused child widget
   VWidget *CurrentFocusChild;
 
   VFont *Font;
 
-  //  Text alignements
-  vuint8        HAlign;
-  vuint8        VAlign;
+  // text alignements
+  vuint8 HAlign;
+  vuint8 VAlign;
 
-  //  Text cursor
-  int         LastX;
-  int         LastY;
+  // text cursor
+  int LastX;
+  int LastY;
 
   // Booleans
-  enum
-  {
-    //  Is this widget visible?
+  enum {
+    // is this widget visible?
     WF_IsVisible    = 0x0001,
-    //  A flag that enables or disables Tick event.
-    WF_TickEnabled    = 0x0002,
-    //  Is this widget enabled and can receive input.
+    // a flag that enables or disables Tick event
+    WF_TickEnabled  = 0x0002,
+    // is this widget enabled and can receive input
     WF_IsEnabled    = 0x0004,
-    //  Can this widget be focused?
-    WF_IsFocusable    = 0x0008,
-    //  Mouse button state for click events.
+    // can this widget be focused?
+    WF_IsFocusable  = 0x0008,
+    // mouse button state for click events
     WF_LMouseDown   = 0x0010,
     WF_MMouseDown   = 0x0020,
     WF_RMouseDown   = 0x0040,
-    //  Shadowed text
-    WF_TextShadowed   = 0x0080,
+    // shadowed text
+    WF_TextShadowed = 0x0080,
   };
-  vuint32       WidgetFlags;
+  vuint32 WidgetFlags;
 
-  VObjectDelegate   FocusLost;
-  VObjectDelegate   FocusReceived;
+  VObjectDelegate FocusLost;
+  VObjectDelegate FocusReceived;
 
-  VObjectDelegate   KeyDown;
-  VObjectDelegate   KeyUp;
+  VObjectDelegate KeyDown;
+  VObjectDelegate KeyUp;
 
-  void AddChild(VWidget*);
-  void RemoveChild(VWidget*);
+  void AddChild (VWidget *);
+  void RemoveChild (VWidget *);
 
-  void ClipTree();
-  void DrawTree();
-  void TickTree(float DeltaTime);
+  void ClipTree ();
+  void DrawTree ();
+  void TickTree (float DeltaTime);
 
-  void FindNewFocus();
+  void FindNewFocus ();
 
-  VWidget *GetWidgetAt(float, float);
+  VWidget *GetWidgetAt (float, float);
 
-  bool TransferAndClipRect(float&, float&, float&, float&, float&, float&,
-    float&, float&) const;
-  void DrawString(int, int, const VStr&, int, int, float);
+  bool TransferAndClipRect (float&, float&, float&, float&, float&, float&, float&, float&) const;
+  void DrawString (int, int, const VStr&, int, int, float);
 
   friend class VRootWidget;
 
 public:
-  //  Destroys all child widgets.
-  virtual void Init(VWidget*);
-  virtual void Destroy() override;
-  void DestroyAllChildren();
+  // destroys all child widgets
+  virtual void Init (VWidget *);
+  virtual void Destroy () override;
+
+  void DestroyAllChildren ();
 
   VRootWidget *GetRootWidget();
 
-  //  Methods to move widget on top or bottom.
-  void Lower();
-  void Raise();
-  void MoveBefore(VWidget*);
-  void MoveAfter(VWidget*);
+  // methods to move widget on top or bottom
+  void Lower ();
+  void Raise ();
+  void MoveBefore (VWidget *);
+  void MoveAfter (VWidget *);
 
-  //  Methods to set position, size and scale.
-  void SetPos(int NewX, int NewY)
-  {
-    SetConfiguration(NewX, NewY, SizeWidth, SizeHeight, SizeScaleX,
-      SizeScaleY);
-  }
-  void SetX(int NewX)
-  {
-    SetPos(NewX, PosY);
-  }
-  void SetY(int NewY)
-  {
-    SetPos(PosX, NewY);
-  }
-  void SetOfsX(int NewX)
-  {
-    if (OfsX != NewX) { OfsX = NewX; SetConfiguration(PosX, PosY, SizeWidth, SizeHeight, SizeScaleX, SizeScaleY); }
-  }
-  void SetOfsY(int NewY)
-  {
-    if (OfsY != NewY) { OfsY = NewY; SetConfiguration(PosX, PosY, SizeWidth, SizeHeight, SizeScaleX, SizeScaleY); }
-  }
-  void SetSize(int NewWidth, int NewHeight)
-  {
-    SetConfiguration(PosX, PosY, NewWidth, NewHeight, SizeScaleX,
-      SizeScaleY);
-  }
-  void SetWidth(int NewWidth)
-  {
-    SetSize(NewWidth, SizeHeight);
-  }
-  void SetHeight(int NewHeight)
-  {
-    SetSize(SizeWidth, NewHeight);
-  }
-  void SetScale(float NewScaleX, float NewScaleY)
-  {
-    SetConfiguration(PosX, PosY, SizeWidth, SizeHeight, NewScaleX, NewScaleY);
-  }
+  // methods to set position, size and scale
+  inline void SetPos (int NewX, int NewY) { SetConfiguration(NewX, NewY, SizeWidth, SizeHeight, SizeScaleX, SizeScaleY); }
+  inline void SetX (int NewX) { SetPos(NewX, PosY); }
+  inline void SetY (int NewY) { SetPos(PosX, NewY); }
+  inline void SetOfsX (int NewX) { if (OfsX != NewX) { OfsX = NewX; SetConfiguration(PosX, PosY, SizeWidth, SizeHeight, SizeScaleX, SizeScaleY); } }
+  inline void SetOfsY (int NewY) { if (OfsY != NewY) { OfsY = NewY; SetConfiguration(PosX, PosY, SizeWidth, SizeHeight, SizeScaleX, SizeScaleY); } }
+  inline void SetSize (int NewWidth, int NewHeight) { SetConfiguration(PosX, PosY, NewWidth, NewHeight, SizeScaleX, SizeScaleY); }
+  inline void SetWidth (int NewWidth) { SetSize(NewWidth, SizeHeight); }
+  inline void SetHeight (int NewHeight) { SetSize(SizeWidth, NewHeight); }
+  inline void SetScale (float NewScaleX, float NewScaleY) { SetConfiguration(PosX, PosY, SizeWidth, SizeHeight, NewScaleX, NewScaleY); }
   void SetConfiguration(int, int, int, int, float = 1.0, float = 1.0);
 
-  //  Visibility methods.
-  void SetVisibility(bool);
-  void Show()
-  {
-    SetVisibility(true);
-  }
-  void Hide()
-  {
-    SetVisibility(false);
-  }
-  bool IsVisible(bool bRecurse = true) const
-  {
-    if (bRecurse)
-    {
+  // visibility methods
+  void SetVisibility (bool);
+  inline void Show () { SetVisibility(true); }
+  inline void Hide () { SetVisibility(false); }
+
+  inline bool IsVisible (bool bRecurse = true) const {
+    if (bRecurse) {
       const VWidget *pParent = this;
-      while (pParent)
-      {
-        if (!(pParent->WidgetFlags & WF_IsVisible))
-        {
-          break;
-        }
+      while (pParent) {
+        if (!(pParent->WidgetFlags&WF_IsVisible)) break;
         pParent = pParent->ParentWidget;
       }
       return (pParent ? false : true);
-    }
-    else
-    {
-      return !!(WidgetFlags & WF_IsVisible);
+    } else {
+      return !!(WidgetFlags&WF_IsVisible);
     }
   }
 
-  //  Enable state methods
-  void SetEnabled(bool);
-  void Enable()
-  {
-    SetEnabled(true);
-  }
-  void Disable()
-  {
-    SetEnabled(false);
-  }
-  bool IsEnabled(bool bRecurse = true) const
-  {
-    if (bRecurse)
-    {
+  // enable state methods
+  void SetEnabled (bool);
+  inline void Enable () { SetEnabled(true); }
+  inline void Disable () { SetEnabled(false); }
+
+  inline bool IsEnabled (bool bRecurse = true) const {
+    if (bRecurse) {
       const VWidget *pParent = this;
-      while (pParent)
-      {
-        if (!(pParent->WidgetFlags & WF_IsEnabled))
-        {
-          break;
-        }
+      while (pParent) {
+        if (!(pParent->WidgetFlags&WF_IsEnabled)) break;
         pParent = pParent->ParentWidget;
       }
       return (pParent ? false : true);
-    }
-    else
-    {
-      return !!(WidgetFlags & WF_IsEnabled);
+    } else {
+      return !!(WidgetFlags&WF_IsEnabled);
     }
   }
 
-  //  Focusable state methods.
-  void SetFocusable(bool);
-  bool IsFocusable() const
-  {
-    return !!(WidgetFlags & WF_IsFocusable);
-  }
+  // focusable state methods
+  void SetFocusable (bool);
+  inline bool IsFocusable () const { return !!(WidgetFlags&WF_IsFocusable); }
 
-  //  Focus methods.
-  void SetCurrentFocusChild(VWidget*);
-  VWidget *GetCurrentFocus() const
-  {
-    return CurrentFocusChild;
-  }
-  bool IsFocus(bool Recurse = true) const;
-  void SetFocus();
+  // focus methods
+  void SetCurrentFocusChild (VWidget *);
+  inline VWidget *GetCurrentFocus () const { return CurrentFocusChild; }
+  bool IsFocus (bool Recurse = true) const;
+  void SetFocus ();
 
-  void DrawPic(int, int, int, float = 1.0, int = 0);
-  void DrawPic(int, int, VTexture*, float = 1.0, int = 0);
-  void DrawShadowedPic(int, int, int);
-  void DrawShadowedPic(int, int, VTexture*);
-  void FillRectWithFlat(int, int, int, int, VName);
-  void FillRect(int, int, int, int, int);
-  void ShadeRect(int, int, int, int, float);
+  void DrawPic (int, int, int, float = 1.0, int = 0);
+  void DrawPic (int, int, VTexture *, float = 1.0, int = 0);
+  void DrawShadowedPic (int, int, int);
+  void DrawShadowedPic (int, int, VTexture *);
+  void FillRectWithFlat (int, int, int, int, VName);
+  void FillRect (int, int, int, int, int);
+  void ShadeRect (int, int, int, int, float);
 
-  void SetFont(VFont*);
-  void SetFont(VName);
-  void SetTextAlign(halign_e, valign_e);
-  void SetTextShadow(bool);
-  void DrawText(int, int, const VStr&, int, int, float);
-  void DrawCursor();
-  void DrawCursorAt(int, int);
+  void SetFont (VFont*);
+  void SetFont (VName);
+  void SetTextAlign (halign_e, valign_e);
+  void SetTextShadow (bool);
+  void DrawText (int, int, const VStr &, int, int, float);
+  void DrawCursor ();
+  void DrawCursorAt (int, int);
 
-  static VWidget *CreateNewWidget(VClass*, VWidget*);
+  static VWidget *CreateNewWidget (VClass *, VWidget *);
 
-  //  Events.
-  void OnCreate()
-  {
+  // events
+  void OnCreate () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnCreate);
   }
-  void OnDestroy()
-  {
+  void OnDestroy () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnDestroy);
   }
-  virtual void OnChildAdded(VWidget *Child)
-  {
+  virtual void OnChildAdded (VWidget *Child) {
     P_PASS_SELF;
     P_PASS_REF(Child);
     EV_RET_VOID(NAME_OnChildAdded);
   }
-  virtual void OnChildRemoved(VWidget *Child)
-  {
+  virtual void OnChildRemoved (VWidget *Child) {
     P_PASS_SELF;
     P_PASS_REF(Child);
     EV_RET_VOID(NAME_OnChildRemoved);
   }
-  virtual void OnConfigurationChanged()
-  {
+  virtual void OnConfigurationChanged () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnConfigurationChanged);
   }
-  virtual void OnVisibilityChanged(bool NewVisibility)
-  {
+  virtual void OnVisibilityChanged (bool NewVisibility) {
     P_PASS_SELF;
     P_PASS_BOOL(NewVisibility);
     EV_RET_VOID(NAME_OnVisibilityChanged);
   }
-  virtual void OnEnableChanged(bool bNewEnable)
-  {
+  virtual void OnEnableChanged (bool bNewEnable) {
     P_PASS_SELF;
     P_PASS_BOOL(bNewEnable);
     EV_RET_VOID(NAME_OnEnableChanged);
   }
-  virtual void OnFocusableChanged(bool bNewFocusable)
-  {
+  virtual void OnFocusableChanged (bool bNewFocusable) {
     P_PASS_SELF;
     P_PASS_BOOL(bNewFocusable);
     EV_RET_VOID(NAME_OnFocusableChanged);
   }
-  virtual void OnFocusReceived()
-  {
+  virtual void OnFocusReceived () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnFocusReceived);
   }
-  virtual void OnFocusLost()
-  {
+  virtual void OnFocusLost () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnFocusLost);
   }
-  virtual void OnDraw()
-  {
+  virtual void OnDraw () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnDraw);
   }
-  virtual void OnPostDraw()
-  {
+  virtual void OnPostDraw () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnPostDraw);
   }
-  virtual void Tick(float DeltaTime)
-  {
+  virtual void Tick (float DeltaTime) {
     P_PASS_SELF;
     P_PASS_FLOAT(DeltaTime);
     EV_RET_VOID(NAME_Tick);
   }
-  virtual bool OnKeyDown(int Key)
-  {
+  virtual bool OnKeyDown (int Key) {
     P_PASS_SELF;
     P_PASS_INT(Key);
     EV_RET_BOOL(NAME_OnKeyDown);
   }
-  virtual bool OnKeyUp(int Key)
-  {
+  virtual bool OnKeyUp (int Key) {
     P_PASS_SELF;
     P_PASS_INT(Key);
     EV_RET_BOOL(NAME_OnKeyUp);
   }
-  virtual bool OnMouseMove(int OldX, int OldY, int NewX, int NewY)
-  {
+  virtual bool OnMouseMove (int OldX, int OldY, int NewX, int NewY) {
     P_PASS_SELF;
     P_PASS_INT(OldX);
     P_PASS_INT(OldY);
@@ -372,55 +293,48 @@ public:
     P_PASS_INT(NewY);
     EV_RET_BOOL(NAME_OnMouseMove);
   }
-  virtual void OnMouseEnter()
-  {
+  virtual void OnMouseEnter () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnMouseEnter);
   }
-  virtual void OnMouseLeave()
-  {
+  virtual void OnMouseLeave () {
     P_PASS_SELF;
     EV_RET_VOID(NAME_OnMouseEnter);
   }
-  virtual bool OnMouseDown(int X, int Y, int Button)
-  {
+  virtual bool OnMouseDown (int X, int Y, int Button) {
     P_PASS_SELF;
     P_PASS_INT(X);
     P_PASS_INT(Y);
     P_PASS_INT(Button);
     EV_RET_BOOL(NAME_OnMouseDown);
   }
-  virtual bool OnMouseUp(int X, int Y, int Button)
-  {
+  virtual bool OnMouseUp (int X, int Y, int Button) {
     P_PASS_SELF;
     P_PASS_INT(X);
     P_PASS_INT(Y);
     P_PASS_INT(Button);
     EV_RET_BOOL(NAME_OnMouseUp);
   }
-  virtual void OnMouseClick(int X, int Y)
-  {
+  virtual void OnMouseClick (int X, int Y) {
     P_PASS_SELF;
     P_PASS_INT(X);
     P_PASS_INT(Y);
     EV_RET_VOID(NAME_OnMouseClick);
   }
-  virtual void OnMMouseClick(int X, int Y)
-  {
+  virtual void OnMMouseClick (int X, int Y) {
     P_PASS_SELF;
     P_PASS_INT(X);
     P_PASS_INT(Y);
     EV_RET_VOID(NAME_OnMMouseClick);
   }
-  virtual void OnRMouseClick(int X, int Y)
-  {
+  virtual void OnRMouseClick (int X, int Y) {
     P_PASS_SELF;
     P_PASS_INT(X);
     P_PASS_INT(Y);
     EV_RET_VOID(NAME_OnRMouseClick);
   }
 
-  //  Script natives
+  // script natives
   DECLARE_FUNCTION(NewChild)
   DECLARE_FUNCTION(Destroy)
   DECLARE_FUNCTION(DestroyAllChildren)
