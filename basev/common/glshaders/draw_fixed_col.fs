2c535a986a0334ba5bc72b281e1865ee5d00899e
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-10-02 03:35:39 +0300

    shader: fixed fixed color shader (do premultiply)

--- basev/common/glshaders/draw_fixed_col.fs
+++ basev/common/glshaders/draw_fixed_col.fs
@@ -2,7 +2,13 @@
 
 uniform vec4 Colour;
 
-void main ()
-{
-  gl_FragColor = Colour;
+
+void main () {
+  // we got a non-premultiplied color, convert it
+  vec4 FinalColor_1;
+  FinalColor_1.r = Colour.r*Colour.a;
+  FinalColor_1.g = Colour.g*Colour.a;
+  FinalColor_1.b = Colour.b*Colour.a;
+  FinalColor_1.a = Colour.a;
+  gl_FragColor = FinalColor_1;
 }
