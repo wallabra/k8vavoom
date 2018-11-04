04ab58e3ac8c7b9d09efca5338439e7c89a7e3a9
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-11-04 04:11:44 +0200

    ajbsp: lowered DISP_EPSILON a little

--- source/ajbsp/bsp.h
+++ source/ajbsp/bsp.h
@@ -624,7 +624,8 @@ sector_t * VertexCheckOpen(vertex_t *vert, double dx, double dy);
 
 
 // smallest distance between two points before being considered equal
-#define DIST_EPSILON  (1.0 / 128.0)
+//#define DIST_EPSILON  (1.0 / 128.0) //k8: old one
+#define DIST_EPSILON  (1.0 / 256.0)
 
 // smallest degrees between two angles before being considered equal
 #define ANG_EPSILON  (1.0 / 1024.0)
