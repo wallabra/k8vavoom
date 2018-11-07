0140dacbf3b28e6a9d566977977af4dd6354e9e8
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-11-07 05:32:40 +0200

    engine: cosmetix

--- source/p_acs_extfunc.h
+++ source/p_acs_extfunc.h
@@ -17,14 +17,14 @@
   ACS_EXTFUNC(SetAirSupply)
   ACS_EXTFUNC(SetSkyScrollSpeed)
   ACS_EXTFUNC(GetArmorType)
-  ACS_EXTFUNC(SpawnSpotForced)
+  ACS_EXTFUNC(SpawnSpotForced) // implemented
   ACS_EXTFUNC(SpawnSpotFacingForced)
   ACS_EXTFUNC(CheckActorProperty)
   ACS_EXTFUNC(SetActorVelocity)
   ACS_EXTFUNC(SetUserVariable)
   ACS_EXTFUNC(GetUserVariable)
   ACS_EXTFUNC(Radius_Quake2)
-  ACS_EXTFUNC(CheckActorClass)
+  ACS_EXTFUNC(CheckActorClass) // implemented
   ACS_EXTFUNC(SetUserArray)
   ACS_EXTFUNC(GetUserArray)
   ACS_EXTFUNC(SoundSequenceOnActor)
@@ -34,9 +34,9 @@
   ACS_EXTFUNC(GetPolyobjY)
   ACS_EXTFUNC(CheckSight)
   ACS_EXTFUNC(SpawnForced)
-  ACS_EXTFUNC(AnnouncerSound)  // Skulltag
+  ACS_EXTFUNC(AnnouncerSound) // skulltag
   ACS_EXTFUNC(SetPointer)
-  ACS_EXTFUNC(ACS_NamedExecute)
+  ACS_EXTFUNC(ACS_NamedExecute) // implemented
   ACS_EXTFUNC(ACS_NamedSuspend)
   ACS_EXTFUNC(ACS_NamedTerminate)
   ACS_EXTFUNC(ACS_NamedLockedExecute)
@@ -58,45 +58,45 @@
   ACS_EXTFUNC(GetUserCVarString)
   ACS_EXTFUNC(SetUserCVarString)
   ACS_EXTFUNC(LineAttack)
-  ACS_EXTFUNC(PlaySound)
-  ACS_EXTFUNC(StopSound)
+  ACS_EXTFUNC(PlaySound) // implemented
+  ACS_EXTFUNC(StopSound) // implemented
   ACS_EXTFUNC(strcmp)
   ACS_EXTFUNC(stricmp)
   ACS_EXTFUNC(StrLeft)
   ACS_EXTFUNC(StrRight)
   ACS_EXTFUNC(StrMid)
-  ACS_EXTFUNC(GetActorClass)
+  ACS_EXTFUNC(GetActorClass) // implemented
   ACS_EXTFUNC(GetWeapon)
   ACS_EXTFUNC(SoundVolume)
   ACS_EXTFUNC(PlayActorSound)
   ACS_EXTFUNC(SpawnDecal)
   ACS_EXTFUNC(CheckFont)
   ACS_EXTFUNC(DropItem)
-  ACS_EXTFUNC(CheckFlag)
+  ACS_EXTFUNC(CheckFlag) // implemented
   ACS_EXTFUNC(SetLineActivation)
   ACS_EXTFUNC(GetLineActivation)
   ACS_EXTFUNC(GetActorPowerupTics)
   ACS_EXTFUNC(ChangeActorAngle)
-  ACS_EXTFUNC(ChangeActorPitch)    // 80
+  ACS_EXTFUNC(ChangeActorPitch) // 80
   ACS_EXTFUNC(GetArmorInfo)
   ACS_EXTFUNC(DropInventory)
   ACS_EXTFUNC(PickActor)
   ACS_EXTFUNC(IsPointerEqual)
   ACS_EXTFUNC(CanRaiseActor)
-  ACS_EXTFUNC(SetActorTeleFog)   // 86
+  ACS_EXTFUNC(SetActorTeleFog) // 86
   ACS_EXTFUNC(SwapActorTeleFog)
   ACS_EXTFUNC(SetActorRoll)
   ACS_EXTFUNC(ChangeActorRoll)
   ACS_EXTFUNC(GetActorRoll)
   ACS_EXTFUNC(QuakeEx)
-  ACS_EXTFUNC(Warp)          // 92
+  ACS_EXTFUNC(Warp) // 92
   ACS_EXTFUNC(GetMaxInventory)
   ACS_EXTFUNC(SetSectorDamage)
   ACS_EXTFUNC(SetSectorTerrain)
   ACS_EXTFUNC(SpawnParticle)
   ACS_EXTFUNC(SetMusicVolume)
   ACS_EXTFUNC(CheckProximity)
-  ACS_EXTFUNC(CheckActorState)   // 99
+  ACS_EXTFUNC(CheckActorState) // 99
   /* Zandronum's - these must be skipped when we reach 99!
   -100:ResetMap(0)
   -101 : PlayerIsSpectator(1)
@@ -109,7 +109,7 @@
 
   ACS_EXTFUNC_NUM(CheckClass, 200)
   ACS_EXTFUNC(DamageActor) // [arookas]
-  ACS_EXTFUNC(SetActorFlag)
+  ACS_EXTFUNC(SetActorFlag) // implemented
   ACS_EXTFUNC(SetTranslation)
   ACS_EXTFUNC(GetActorFloorTexture)
   ACS_EXTFUNC(GetActorFloorTerrain)
@@ -120,15 +120,14 @@
   ACS_EXTFUNC(ScriptCall)
   ACS_EXTFUNC(StartSlideshow)
 
-    // Eternity's
+  // Eternity's
   ACS_EXTFUNC_NUM(GetLineX, 300)
   ACS_EXTFUNC(GetLineY)
-
 
   // OpenGL stuff
   ACS_EXTFUNC_NUM(SetSectorGlow, 400)
   ACS_EXTFUNC(SetFogDensity)
 
   // ZDaemon
-  ACS_EXTFUNC_NUM(GetTeamScore, 19620)  // (int team)
-  ACS_EXTFUNC(SetTeamScore)      // (int team, int value
+  ACS_EXTFUNC_NUM(GetTeamScore, 19620) // (int team)
+  ACS_EXTFUNC(SetTeamScore) // (int team, int value)
