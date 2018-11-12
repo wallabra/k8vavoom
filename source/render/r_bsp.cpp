b50181cc99237715fb22686f2a34028b999f2c09
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-11-12 19:43:46 +0200

    engine: removed unnecessary console message

--- source/render/r_bsp.cpp
+++ source/render/r_bsp.cpp
@@ -813,7 +813,7 @@ void VRenderLevelShared::RenderPortals () {
     if (oldMaxMirrors != r_maxmirrors || oldPortalDepth != r_max_portal_depth ||
         oldHorizons != r_allow_horizons || oldMirrors != r_allow_mirrors)
     {
-      GCon->Logf("portal settings changed, resetting portal info...");
+      //GCon->Logf("portal settings changed, resetting portal info...");
       for (int i = 0; i < Portals.Num(); ++i) {
         if (Portals[i]) {
           delete Portals[i];
