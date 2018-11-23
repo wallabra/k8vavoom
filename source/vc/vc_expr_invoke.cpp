c7ea57977f2eaeac24f056f4dd209c51d2005deb
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-11-23 20:41:34 +0200

    vc: decorate: cosmetix

--- source/vc/vc_expr_invoke.cpp
+++ source/vc/vc_expr_invoke.cpp
@@ -2526,7 +2526,7 @@ void VInvocation::CheckDecorateParams (VEmitContext &ec) {
             if (lbl < 0) {
               ParseError(ALoc, "`%s` argument #%d is something fucked: '%s'", Func->GetName(), i+1, str);
             } else {
-              ParseWarning(ALoc, "`%s` argument #%d should be number; FIX YOUR BROKEN CODE!", Func->GetName(), i+1);
+              ParseWarning(ALoc, "`%s` argument #%d should be number %d; FIX YOUR BROKEN CODE!", Func->GetName(), i+1, lbl);
               delete Args[i];
               Args[i] = new VIntLiteral(lbl, ALoc);
             }
