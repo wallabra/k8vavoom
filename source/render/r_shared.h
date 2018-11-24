921f77fa038469742ca3372839b4e2cbe6cadf95
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-11-25 01:07:57 +0200

    engine: cosmetix

--- source/render/r_shared.h
+++ source/render/r_shared.h
@@ -26,20 +26,15 @@
 //**  Defines shared by refresh and drawer
 //**
 //**************************************************************************
-
 #ifndef _R_SHARED_H
 #define _R_SHARED_H
-
-// HEADER FILES ------------------------------------------------------------
 
 #include "fmd2defs.h"
 #include "drawer.h"
 
-// MACROS ------------------------------------------------------------------
 
-//  Colour maps
-enum
-{
+// colour maps
+enum {
   CM_Default,
   CM_Inverse,
   CM_Gold,
@@ -49,222 +44,208 @@ enum
   CM_Max
 };
 
-//  Simulate light fading using dark fog
-enum
-{
+// simulate light fading using dark fog
+enum {
   FADE_LIGHT = 0xff010101
 };
 
-// TYPES -------------------------------------------------------------------
 
-class TClipPlane : public TPlane
-{
+// ////////////////////////////////////////////////////////////////////////// //
+class TClipPlane : public TPlane {
 public:
-  TClipPlane    *next;
+  TClipPlane *next;
 
-  int       clipflag;
-  TVec      enter;
-  TVec      exit;
-  int       entered;
-  int       exited;
+  int clipflag;
+  TVec enter;
+  TVec exit;
+  int entered;
+  int exited;
 };
 
-struct texinfo_t
-{
-  TVec      saxis;
-  float     soffs;
-  TVec      taxis;
-  float     toffs;
+struct texinfo_t {
+  TVec saxis;
+  float soffs;
+  TVec taxis;
+  float toffs;
   VTexture *Tex;
   bool noDecals;
-  //  1.1 for solid surfaces
-  // Alpha for masked surfaces
-  float     Alpha;
-  bool      Additive;
-  vuint8      ColourMap;
+  // 1.1 for solid surfaces
+  // alpha for masked surfaces
+  float Alpha;
+  bool Additive;
+  vuint8 ColourMap;
 };
 
-struct surface_t
-{
+struct surface_t {
   surface_t *next;
   surface_t *DrawNext;
   texinfo_t *texinfo;
   TPlane *plane;
   sec_plane_t *HorizonPlane;
-  vuint32     Light;    //  Light level and colour.
-  vuint32     Fade;
+  vuint32 Light; // light level and colour
+  vuint32 Fade;
   vuint8 *lightmap;
   rgb_t *lightmap_rgb;
-  int       dlightframe;
-  int       dlightbits;
-  int       count;
-  short     texturemins[2];
-  short     extents[2];
+  int dlightframe;
+  int dlightbits;
+  int count;
+  short texturemins[2];
+  short extents[2];
   surfcache_t *CacheSurf;
   seg_t *dcseg; // seg with decals for this surface
-  TVec      verts[1];
+  TVec verts[1]; // dynamic array
 };
 
-//
-//  Camera texture.
-//
-class VCameraTexture : public VTexture
-{
+
+// ////////////////////////////////////////////////////////////////////////// //
+// camera texture
+class VCameraTexture : public VTexture {
 public:
   vuint8 *Pixels;
-  bool    bNeedsUpdate;
-  bool    bUpdated;
+  bool bNeedsUpdate;
+  bool bUpdated;
 
-  VCameraTexture(VName, int, int);
-  virtual ~VCameraTexture() override;
-  virtual bool CheckModified() override;
-  virtual vuint8 *GetPixels() override;
-  virtual void Unload() override;
-  void CopyImage();
-  virtual VTexture *GetHighResolutionTexture() override;
+  VCameraTexture (VName, int, int);
+  virtual ~VCameraTexture () override;
+  virtual bool CheckModified () override;
+  virtual vuint8 *GetPixels () override;
+  virtual void Unload () override;
+  void CopyImage ();
+  virtual VTexture *GetHighResolutionTexture () override;
 };
+
 
 class VRenderLevelShared;
 
-//
-//  Base class for portals.
-//
-class VPortal
-{
+
+// ////////////////////////////////////////////////////////////////////////// //
+// base class for portals
+class VPortal {
 public:
   VRenderLevelShared *RLev;
-  TArray<surface_t*>    Surfs;
-  int           Level;
+  TArray<surface_t *> Surfs;
+  int Level;
 
-  VPortal(VRenderLevelShared *ARLev);
-  virtual ~VPortal();
-  virtual bool NeedsDepthBuffer() const;
-  virtual bool IsSky() const;
-  virtual bool MatchSky(class VSky*) const;
-  virtual bool MatchSkyBox(VEntity*) const;
-  virtual bool MatchMirror(TPlane*) const;
-  void Draw(bool);
-  virtual void DrawContents() = 0;
+  VPortal (VRenderLevelShared *ARLev);
+  virtual ~VPortal ();
+  virtual bool NeedsDepthBuffer () const;
+  virtual bool IsSky () const;
+  virtual bool MatchSky (class VSky *) const;
+  virtual bool MatchSkyBox (VEntity *) const;
+  virtual bool MatchMirror (TPlane *) const;
+  void Draw (bool);
+  virtual void DrawContents () = 0;
 
 protected:
-  void SetUpRanges(VViewClipper&, bool);
+  void SetUpRanges (VViewClipper &, bool);
 };
 
-struct VMeshFrame
-{
+
+// ////////////////////////////////////////////////////////////////////////// //
+struct VMeshFrame {
   TVec *Verts;
   TVec *Normals;
   TPlane *Planes;
-  vuint32     VertsOffset;
-  vuint32     NormalsOffset;
+  vuint32 VertsOffset;
+  vuint32 NormalsOffset;
 };
 
-struct VMeshSTVert
-{
-  float     S;
-  float     T;
+struct VMeshSTVert {
+  float S;
+  float T;
 };
 
-struct VMeshTri
-{
-  vuint16       VertIndex[3];
+struct VMeshTri {
+  vuint16 VertIndex[3];
 };
 
-struct VMeshEdge
-{
-  vuint16       Vert1;
-  vuint16       Vert2;
-  vint16        Tri1;
-  vint16        Tri2;
+struct VMeshEdge {
+  vuint16 Vert1;
+  vuint16 Vert2;
+  vint16 Tri1;
+  vint16 Tri2;
 };
 
-struct VMeshModel
-{
-  VStr        Name;
-  mmdl_t *Data;   // only access through Mod_Extradata
-  TArray<VName>   Skins;
-  TArray<VMeshFrame>  Frames;
-  TArray<TVec>    AllVerts;
-  TArray<TVec>    AllNormals;
-  TArray<TPlane>    AllPlanes;
+struct VMeshModel {
+  VStr Name;
+  mmdl_t *Data; // only access through Mod_Extradata
+  TArray<VName> Skins;
+  TArray<VMeshFrame> Frames;
+  TArray<TVec> AllVerts;
+  TArray<TVec> AllNormals;
+  TArray<TPlane> AllPlanes;
   TArray<VMeshSTVert> STVerts;
-  TArray<VMeshTri>  Tris;
+  TArray<VMeshTri> Tris;
   TArray<VMeshEdge> Edges;
-  bool        Uploaded;
-  vuint32       VertsBuffer;
-  vuint32       IndexBuffer;
+  bool Uploaded;
+  vuint32 VertsBuffer;
+  vuint32 IndexBuffer;
 };
 
-// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
 
-void R_DrawViewBorder();
+// ////////////////////////////////////////////////////////////////////////// //
+void R_DrawViewBorder ();
+void R_ParseMapDefSkyBoxesScript (VScriptParser *sc);
+VName R_HasNamedSkybox (const VStr &aname);
 
-// PUBLIC DATA DECLARATIONS ------------------------------------------------
 
-//
-// POV related.
-//
-extern TVec       vieworg;
-extern TVec       viewforward;
-extern TVec       viewright;
-extern TVec       viewup;
-extern TAVec      viewangles;
+// ////////////////////////////////////////////////////////////////////////// //
+// POV related
+extern TVec vieworg;
+extern TVec viewforward;
+extern TVec viewright;
+extern TVec viewup;
+extern TAVec viewangles;
 
-extern VCvarI     r_fog;
-extern VCvarF     r_fog_r;
-extern VCvarF     r_fog_g;
-extern VCvarF     r_fog_b;
-extern VCvarF     r_fog_start;
-extern VCvarF     r_fog_end;
-extern VCvarF     r_fog_density;
+extern VCvarI r_fog;
+extern VCvarF r_fog_r;
+extern VCvarF r_fog_g;
+extern VCvarF r_fog_b;
+extern VCvarF r_fog_start;
+extern VCvarF r_fog_end;
+extern VCvarF r_fog_density;
 
-extern VCvarB     r_vsync;
-extern VCvarB     r_vsync_adaptive;
+extern VCvarB r_vsync;
+extern VCvarB r_vsync_adaptive;
 
-extern VCvarB     r_fade_light;
-extern VCvarF     r_fade_factor;
+extern VCvarB r_fade_light;
+extern VCvarF r_fade_factor;
 
-extern VCvarF     r_sky_bright_factor;
+extern VCvarF r_sky_bright_factor;
 
-extern TClipPlane   view_clipplanes[5];
+extern TClipPlane view_clipplanes[5];
 
-extern bool       MirrorFlip;
-extern bool       MirrorClip;
+extern bool MirrorFlip;
+extern bool MirrorClip;
 
-extern int        r_dlightframecount;
-extern vuint32      blocklightsr[18 * 18];
-extern vuint32      blocklightsg[18 * 18];
-extern vuint32      blocklightsb[18 * 18];
+extern int r_dlightframecount;
+extern vuint32 blocklightsr[18*18];
+extern vuint32 blocklightsg[18*18];
+extern vuint32 blocklightsb[18*18];
 
-extern rgba_t     r_palette[256];
-extern vuint8     r_black_colour;
-extern vuint8     r_white_colour;
+extern rgba_t r_palette[256];
+extern vuint8 r_black_colour;
+extern vuint8 r_white_colour;
 
-extern vuint8     r_rgbtable[32 * 32 * 32 + 4];
+extern vuint8 r_rgbtable[32*32*32+4];
 
 extern int usegamma;
 //extern const vuint8 gammatable[5][256];
 extern const vuint8 *getGammaTable (int idx);
 
+extern float PixelAspect;
 
-extern float      PixelAspect;
-
-extern VTextureTranslation  ColourMaps[CM_Max];
+extern VTextureTranslation ColourMaps[CM_Max];
 
 
 //==========================================================================
 //
-//  R_LookupRBG
+//  R_LookupRGB
 //
 //==========================================================================
-
-static inline int R_LookupRGB(vuint8 r, vuint8 g, vuint8 b)
-{
-  return r_rgbtable[((r << 7) & 0x7c00) + ((g << 2) & 0x3e0) +
-    ((b >> 3) & 0x1f)];
+static inline int R_LookupRGB (vuint8 r, vuint8 g, vuint8 b) {
+  return r_rgbtable[((r<<7)&0x7c00)+((g<<2)&0x3e0)+((b>>3)&0x1f)];
 }
 
-void R_ParseMapDefSkyBoxesScript (VScriptParser *sc);
-VName R_HasNamedSkybox (const VStr &aname);
 
 #endif
