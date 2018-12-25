37a475a4501c4204c8e267266ad024af00597252
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-12-25 04:44:25 +0200

    engine: particle code cosmetix

--- source/render/r_particle.cpp
+++ source/render/r_particle.cpp
@@ -26,51 +26,26 @@
 //**  Rendering of particles.
 //**
 //**************************************************************************
-
-// HEADER FILES ------------------------------------------------------------
-
 #include "gamedefs.h"
 #include "r_local.h"
 
-// MACROS ------------------------------------------------------------------
 
-// TYPES -------------------------------------------------------------------
+VCvarB r_draw_particles("r_draw_particles", true, "Draw particles?", CVAR_Archive);
 
-// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
-
-// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
-
-// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
-
-// EXTERNAL DATA DECLARATIONS ----------------------------------------------
-
-// PUBLIC DATA DEFINITIONS -------------------------------------------------
-
-VCvarB        r_draw_particles("r_draw_particles", true, "Draw particles?", CVAR_Archive);
-
-// PRIVATE DATA DEFINITIONS ------------------------------------------------
-
-// CODE --------------------------------------------------------------------
 
 //==========================================================================
 //
 //  VRenderLevelShared::InitParticles
 //
 //==========================================================================
-
-void VRenderLevelShared::InitParticles()
-{
+void VRenderLevelShared::InitParticles () {
   guard(VRenderLevelShared::InitParticles);
   const char *p = GArgs.CheckValue("-particles");
 
-  if (p)
-  {
+  if (p) {
     NumParticles = atoi(p);
-    if (NumParticles < ABSOLUTE_MIN_PARTICLES)
-      NumParticles = ABSOLUTE_MIN_PARTICLES;
-  }
-  else
-  {
+    if (NumParticles < ABSOLUTE_MIN_PARTICLES) NumParticles = ABSOLUTE_MIN_PARTICLES;
+  } else {
     NumParticles = MAX_PARTICLES;
   }
 
@@ -78,121 +53,97 @@ void VRenderLevelShared::InitParticles()
   unguard;
 }
 
+
 //==========================================================================
 //
 //  VRenderLevelShared::ClearParticles
 //
 //==========================================================================
-
-void VRenderLevelShared::ClearParticles()
-{
+void VRenderLevelShared::ClearParticles () {
   guard(VRenderLevelShared::ClearParticles);
   FreeParticles = &Particles[0];
   ActiveParticles = nullptr;
-
-  for (int i = 0; i < NumParticles; i++)
-    Particles[i].next = &Particles[i + 1];
-  Particles[NumParticles - 1].next = nullptr;
+  for (int i = 0; i < NumParticles; ++i) Particles[i].next = &Particles[i+1];
+  Particles[NumParticles-1].next = nullptr;
   unguard;
 }
+
 
 //==========================================================================
 //
 //  VRenderLevelShared::NewParticle
 //
 //==========================================================================
-
-particle_t *VRenderLevelShared::NewParticle()
-{
+particle_t *VRenderLevelShared::NewParticle () {
   guard(VRenderLevelShared::NewParticle);
-  if (!FreeParticles)
-  {
-    //  No free particles
-    return nullptr;
-  }
-  //  Remove from list of free particles
+  if (!FreeParticles) return nullptr; // no free particles
+  if (!r_draw_particles) return nullptr; // save some resources
+  // remove from list of free particles
   particle_t *p = FreeParticles;
   FreeParticles = p->next;
-  //  Clean
+  // clean
   memset((void *)p, 0, sizeof(*p));
-  //  Add to active particles
+  // add to active particles
   p->next = ActiveParticles;
   ActiveParticles = p;
   return p;
   unguard;
 }
 
+
 //==========================================================================
 //
 //  VRenderLevelShared::UpdateParticles
 //
 //==========================================================================
-
-void VRenderLevelShared::UpdateParticles(float frametime)
-{
+void VRenderLevelShared::UpdateParticles (float frametime) {
   guard(VRenderLevelShared::UpdateParticles);
-  particle_t    *p, *kill;
+  particle_t *p, *kill;
 
-  if (GGameInfo->IsPaused() ||
-    (Level->LevelInfo->LevelInfoFlags2 & VLevelInfo::LIF2_Frozen))
-  {
-    return;
-  }
+  if (GGameInfo->IsPaused() || (Level->LevelInfo->LevelInfoFlags2&VLevelInfo::LIF2_Frozen)) return;
+  if (!r_draw_particles) return; // save some resources
 
   kill = ActiveParticles;
-  while (kill && kill->die < Level->Time)
-  {
+  while (kill && kill->die < Level->Time) {
     ActiveParticles = kill->next;
     kill->next = FreeParticles;
     FreeParticles = kill;
     kill = ActiveParticles;
   }
 
-  for (p = ActiveParticles; p; p = p->next)
-  {
+  for (p = ActiveParticles; p; p = p->next) {
     kill = p->next;
-    while (kill && kill->die < Level->Time)
-    {
+    while (kill && kill->die < Level->Time) {
       p->next = kill->next;
       kill->next = FreeParticles;
       FreeParticles = kill;
       kill = p->next;
     }
 
-    p->org += (p->vel * frametime);
+    p->org += (p->vel*frametime);
     Level->LevelInfo->eventUpdateParticle(p, frametime);
   }
   unguard;
 }
+
 
 //==========================================================================
 //
 //  VRenderLevelShared::DrawParticles
 //
 //==========================================================================
-
-void VRenderLevelShared::DrawParticles()
-{
+void VRenderLevelShared::DrawParticles () {
   guard(VRenderLevelShared::DrawParticles);
-  if (!r_draw_particles)
-  {
-    return;
-  }
+  if (!r_draw_particles) return;
   Drawer->StartParticles();
-  for (particle_t *p = ActiveParticles; p; p = p->next)
-  {
-    if (ColourMap)
-    {
+  for (particle_t *p = ActiveParticles; p; p = p->next) {
+    if (ColourMap) {
       vuint32 Col = p->colour;
-      rgba_t TmpCol = ColourMaps[ColourMap].GetPalette()[R_LookupRGB(
-        (Col >> 16) & 255, (Col >> 8) & 255, Col & 255)];
-      p->colour = (Col & 0xff000000) | (TmpCol.r << 16) |
-        (TmpCol.g << 8) | TmpCol.b;
+      rgba_t TmpCol = ColourMaps[ColourMap].GetPalette()[R_LookupRGB((Col>>16)&255, (Col>>8)&255, Col&255)];
+      p->colour = (Col&0xff000000)|(TmpCol.r<<16)|(TmpCol.g<<8)|TmpCol.b;
       Drawer->DrawParticle(p);
       p->colour = Col;
-    }
-    else
-    {
+    } else {
       Drawer->DrawParticle(p);
     }
   }
