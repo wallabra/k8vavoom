c12356cf8fb47eca98374650f8ec5b41b85df5dd
Ketmar Dark <ketmar@ketmar.no-ip.org>
2019-02-21 14:31:46 +0200

    corelib: added default console logger

--- libs/core/log.cpp
+++ libs/core/log.cpp
@@ -230,3 +230,71 @@ __attribute__((format(printf, 2, 3))) void VLog::DWriteLine (const char *fmt, ..
   doWrite(NAME_Dev, fmt, ap, true);
   va_end(ap);
 }
+
+
+// ////////////////////////////////////////////////////////////////////////// //
+#if !defined(_WIN32) || defined(IN_WADCHECK)
+class VConLogger : public VLogListener {
+private:
+  bool wasNL;
+  EName lastEvent;
+
+public:
+  VConLogger () : wasNL(true), lastEvent(NAME_None) {
+    VName::StaticInit();
+    GLog.AddListener(this);
+  }
+
+  virtual void Serialise (const char *Text, EName Event) override {
+    //fprintf(stderr, "%s: <%s>\n", *VName(Event), *VStr(Text).quote());
+    while (*Text) {
+      if (lastEvent != Event) {
+        if (!wasNL) fputc('\n', stdout);
+        wasNL = true;
+        lastEvent = Event;
+      }
+      if (Text[0] == '\r' && Text[1] == '\n') ++Text;
+      if (Text[0] == '\n' || Text[0] == '\r') {
+        if (wasNL) {
+          if (Event != NAME_Log) {
+            fputs(*VName(Event), stdout);
+            fputc(':', stdout);
+          }
+        }
+        fputc('\n', stdout);
+        ++Text;
+        wasNL = true;
+        lastEvent = Event;
+      } else {
+        const char *eol0 = strchr(Text, '\n');
+        const char *eol1 = strchr(Text, '\r');
+        if (!eol0 && !eol1) {
+          if (wasNL) {
+            if (Event != NAME_Log) {
+              fputs(*VName(Event), stdout);
+              fputs(": ", stdout);
+            }
+            wasNL = false;
+          }
+          fputs(Text, stdout);
+          return;
+        }
+        const char *eol = (eol0 && eol1 ? (eol0 < eol1 ? eol0 : eol1) : eol0 ? eol0 : eol1);
+        check(eol != Text);
+        // ends with eol
+        if (wasNL) {
+          if (Event != NAME_Log) {
+            fputs(*VName(Event), stdout);
+            fputs(": ", stdout);
+          }
+          wasNL = false;
+        }
+        fwrite(Text, (size_t)(ptrdiff_t)(eol-Text), 1, stdout);
+        Text = eol;
+      }
+    }
+  }
+};
+
+VConLogger conlogger;
+#endif
