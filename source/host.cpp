26eee0f96a686fca68aaa0e2a68eea0f474db472
Ketmar Dark <ketmar@ketmar.no-ip.org>
2019-02-24 21:43:21 +0200

    engine: added build date to startup log

--- source/host.cpp
+++ source/host.cpp
@@ -160,7 +160,7 @@ void Host_Init () {
   */
 
   GCon->Log(NAME_Init, "---------------------------------------------------------------");
-  GCon->Log(NAME_Init, "      (k8)VaVoom Game Engine, started by Janis Legzdinsh");
+  GCon->Logf(NAME_Init, "k8vavoom Game Engine, started by Janis Legzdinsh | %s", __DATE__);
   GCon->Log(NAME_Init, "also starring Francisco Ortega, and others (k8:drop me a note!)");
   GCon->Log(NAME_Init, "Ketmar Dark: improvements, bugfixes, new bugs, segfaults, etc.");
   GCon->Log(NAME_Init, "alot of invaluable help and testing (esp. x86_64): id0");
