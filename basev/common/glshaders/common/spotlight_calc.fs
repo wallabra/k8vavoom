f061d5bb674af0a26e4cb0ec4956d53e277a1dca
Ketmar Dark <ketmar@ketmar.no-ip.org>
2019-04-04 12:29:30 +0300

    base: spotlight shader cleanup

--- basev/common/glshaders/common/spotlight_calc.fs
+++ basev/common/glshaders/common/spotlight_calc.fs
@@ -1,13 +1,6 @@
-  //float attenuation = 1.0/(1.0+(LightRadius-LightMin)*DistToLight);
-  //attenuation *= 255.0;
-  //attenuation = 1.0 / (1.0 + light.attenuation * pow(distanceToLight, 2));
-
   // cone restrictions (affects attenuation)
   vec3 surfaceToLight = normalize(VertToLight);
-  float distanceToLight = length(VertToLight);
-
-  //vec3 ConeDirection = vec3(1.0, 0.0, 0.0);
-  //float ConeAngle = 50.0;
+  //float distanceToLight = length(VertToLight);
 
   //float lightToSurfaceAngle = degrees(acos(dot(-surfaceToLight, normalize(ConeDirection))));
   float lightToSurfaceAngle = degrees(acos(dot(-surfaceToLight, ConeDirection)));
