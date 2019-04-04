30947cac29991ccfc79f2f0f5df2066f4a5ed5db
Ketmar Dark <ketmar@ketmar.no-ip.org>
2019-04-04 10:19:02 +0300

    base: shader: nicer-looking spotlights

--- basev/common/glshaders/common/spotlight_calc.fs
+++ basev/common/glshaders/common/spotlight_calc.fs
@@ -11,4 +11,9 @@
 
   //float lightToSurfaceAngle = degrees(acos(dot(-surfaceToLight, normalize(ConeDirection))));
   float lightToSurfaceAngle = degrees(acos(dot(-surfaceToLight, ConeDirection)));
-  if (lightToSurfaceAngle > ConeAngle) attenuation = 0.0;
+
+  //if (lightToSurfaceAngle > ConeAngle) attenuation = 0.0;
+  //attenuation *= min(0.5, (ConeAngle-lightToSurfaceAngle)/ConeAngle);
+  //attenuation *= clamp((ConeAngle-lightToSurfaceAngle)/ConeAngle, 0.0, 1.0);
+  //attenuation *= smoothstep(0.0, 1.0, clamp((ConeAngle-lightToSurfaceAngle)/ConeAngle, 0.0, 1.0));
+  attenuation *= sin(clamp((ConeAngle-lightToSurfaceAngle)/ConeAngle, 0.0, 1.0)*(3.14159265358979323846/2.0));
