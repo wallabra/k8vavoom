60ed4a513e8e4a31cd6bc8e424177f57b193c1a7
Ketmar Dark <ketmar@ketmar.no-ip.org>
2019-04-05 18:14:25 +0300

    base: nope, brightmaps are not additive

--- basev/common/glshaders/common/brightmap_calc.fs
+++ basev/common/glshaders/common/brightmap_calc.fs
@@ -1,9 +1,11 @@
   vec4 BMColor = texture2D(TextureBM, TextureCoordinate);
   BMColor.rgb *= BMColor.a;
-#if 0
+#if 1
   lt.rgb = max(lt.rgb, BMColor.rgb);
   //lt.rgb = BMColor.rgb;
 #else
   // additive brightmaps
   lt.rgb = min(lt.rgb+BMColor.rgb, 1.0);
+  //lt.rgb = BMColor.rgb;
+  //TexColour.rgb = vec3(1.0, 1.0, 1.0);
 #endif
