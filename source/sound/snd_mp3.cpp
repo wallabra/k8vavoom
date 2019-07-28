42061ce630676c1a26221367923de570802f90ba
Ketmar Dark <ketmar@ketmar.no-ip.org>
2019-07-28 08:39:38 +0300

    engine: sound: mp3 codec cleanups

--- source/sound/snd_mp3.cpp
+++ source/sound/snd_mp3.cpp
@@ -29,337 +29,289 @@
 #include "gamedefs.h"
 #include "snd_local.h"
 
-// MACROS ------------------------------------------------------------------
 
-// TYPES -------------------------------------------------------------------
-
-class VMp3AudioCodec : public VAudioCodec
-{
+class VMp3AudioCodec : public VAudioCodec {
 public:
-  enum { INPUT_BUFFER_SIZE = 5 * 8192 };
+  enum { INPUT_BUFFER_SIZE = 5*8192 };
 
+public:
   VStream *Strm;
-  bool        FreeStream;
-  int         BytesLeft;
-  bool        Initialised;
+  bool FreeStream;
+  int BytesLeft;
+  bool Initialised;
+  // set in `Init()`
+  int StreamStart;
+  int StreamSize;
 
-  mad_stream      Stream;
-  mad_frame     Frame;
-  mad_synth     Synth;
-  vuint8        InputBuffer[INPUT_BUFFER_SIZE + MAD_BUFFER_GUARD];
-  int         FramePos;
-  bool        HaveFrame;
+  mad_stream Stream;
+  mad_frame Frame;
+  mad_synth Synth;
+  vuint8 InputBuffer[INPUT_BUFFER_SIZE+MAD_BUFFER_GUARD];
+  int FramePos;
+  bool HaveFrame;
 
-  VMp3AudioCodec(VStream*, bool);
-  ~VMp3AudioCodec();
-  bool Init();
-  virtual int Decode(short*, int) override;
-  int ReadData();
-  virtual bool Finished() override;
-  virtual void Restart() override;
-  static VAudioCodec *Create(VStream*);
-};
-
-class VMp3SampleLoader : public VSampleLoader
-{
 public:
-  virtual void Load(sfxinfo_t&, VStream&) override;
+  VMp3AudioCodec (VStream *AStrm, bool AFreeStream);
+  virtual ~VMp3AudioCodec () override;
+
+  bool Init ();
+  int ReadData ();
+
+  virtual int Decode (short *Data, int NumSamples) override;
+  virtual bool Finished () override;
+  virtual void Restart () override;
+
+  static VAudioCodec *Create (VStream *InStrm);
 };
 
-// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
 
-// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
+class VMp3SampleLoader : public VSampleLoader {
+public:
+  virtual void Load (sfxinfo_t &Sfx, VStream &Stream) override;
+};
 
-// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
-
-// EXTERNAL DATA DECLARATIONS ----------------------------------------------
-
-// PUBLIC DATA DEFINITIONS -------------------------------------------------
-
-// PRIVATE DATA DEFINITIONS ------------------------------------------------
 
 IMPLEMENT_AUDIO_CODEC(VMp3AudioCodec, "MP3");
 
-VMp3SampleLoader    Mp3SampleLoader;
+VMp3SampleLoader Mp3SampleLoader;
 
-// CODE --------------------------------------------------------------------
 
 //==========================================================================
 //
 //  VMp3AudioCodec::VMp3AudioCodec
 //
 //==========================================================================
-
-VMp3AudioCodec::VMp3AudioCodec(VStream *AStrm, bool AFreeStream)
-: Strm(AStrm)
-, FreeStream(AFreeStream)
-, Initialised(false)
+VMp3AudioCodec::VMp3AudioCodec (VStream *AStrm, bool AFreeStream)
+  : Strm(AStrm)
+  , FreeStream(AFreeStream)
+  , Initialised(false)
+  , StreamStart(0)
+  , StreamSize(0)
 {
-  //  Seek file to the begining
-  BytesLeft = Strm->TotalSize();
+  // seek file to the begining
+  StreamSize = BytesLeft = Strm->TotalSize();
   Strm->Seek(0);
 
-  //  Initialise structures used by libmad
+  // initialise structures used by libmad
   mad_stream_init(&Stream);
   mad_frame_init(&Frame);
   mad_synth_init(&Synth);
 }
+
 
 //==========================================================================
 //
 //  VMp3AudioCodec::~VMp3AudioCodec
 //
 //==========================================================================
-
-VMp3AudioCodec::~VMp3AudioCodec()
-{
-  if (Initialised)
-  {
-    //  Close file only if decoder has been initialised succesfully.
-    if (FreeStream)
-    {
+VMp3AudioCodec::~VMp3AudioCodec () {
+  if (Initialised) {
+    // close file only if decoder has been initialised succesfully.
+    if (FreeStream) {
       Strm->Close();
       delete Strm;
     }
     Strm = nullptr;
   }
-  //  Clear structs used by libmad.
+  // clear structs used by libmad
   mad_synth_finish(&Synth);
   mad_frame_finish(&Frame);
   mad_stream_finish(&Stream);
 }
+
 
 //==========================================================================
 //
 //  VMp3AudioCodec::Init
 //
 //==========================================================================
-
-bool VMp3AudioCodec::Init()
-{
-  //  Check for ID3v2 header.
+bool VMp3AudioCodec::Init () {
+  // check for ID3v2 header
   vuint8 Id3Hdr[10];
   int SavedPos = Strm->Tell();
   Strm->Serialise(Id3Hdr, 10);
-  if (Id3Hdr[0] == 'I' && Id3Hdr[1] == 'D' && Id3Hdr[2] == '3')
-  {
-    //  It's a ID3v3 header, skip it.
-    int HdrSize = Id3Hdr[9] + (Id3Hdr[8] << 7) + (Id3Hdr[7] << 14) +
-      (Id3Hdr[6] << 21);
-    if (HdrSize + 10 > BytesLeft)
-      return false;
-    Strm->Seek(Strm->Tell() + HdrSize);
-    BytesLeft -= 10 + HdrSize;
-  }
-  else
-  {
-    //  Not a ID3v3 header, seek back to saved position.
+  if (Id3Hdr[0] == 'I' && Id3Hdr[1] == 'D' && Id3Hdr[2] == '3') {
+    // it's a ID3v3 header, skip it
+    int HdrSize = Id3Hdr[9]+(Id3Hdr[8]<<7)+(Id3Hdr[7]<<14)+(Id3Hdr[6]<<21);
+    if (HdrSize+10 > BytesLeft) return false;
+    Strm->Seek(Strm->Tell()+HdrSize);
+    BytesLeft -= 10+HdrSize;
+  } else {
+    // not a ID3v3 header, seek back to saved position
     Strm->Seek(SavedPos);
   }
 
-  //  Read some data.
-  ReadData();
+  if (Strm->IsError()) return false;
 
-  //  Decode first frame. If this fails we assume it's not a MP3 file.
-  if (mad_frame_decode(&Frame, &Stream))
-  {
-    //  Not a valid stream.
+  StreamStart = Strm->Tell();
+  StreamSize = BytesLeft;
+  // read some data
+  ReadData();
+  if (Strm->IsError()) return false;
+
+  // decode first frame. If this fails we assume it's not a MP3 file
+  if (mad_frame_decode(&Frame, &Stream)) {
+    // not a valid stream
     return false;
   }
 
-  //  We are ready to read data.
+  // we are ready to read data
   mad_synth_frame(&Synth, &Frame);
   FramePos = 0;
   HaveFrame = true;
 
-  //  Everything's OK.
+  // everything's ok
   SampleRate = Frame.header.samplerate;
   Initialised = true;
   return true;
 }
+
 
 //==========================================================================
 //
 //  VMp3AudioCodec::Decode
 //
 //==========================================================================
-
-int VMp3AudioCodec::Decode(short *Data, int NumSamples)
-{
+int VMp3AudioCodec::Decode (short *Data, int NumSamples) {
   int CurSample = 0;
-  do
-  {
-    if (HaveFrame)
-    {
-      //  Convert stream from fixed point to short.
-      for (; FramePos < Synth.pcm.length; FramePos++)
-      {
-        //  Left channel
+  for (;;) {
+    if (HaveFrame) {
+      // convert stream from fixed point to short
+      for (; FramePos < Synth.pcm.length; ++FramePos) {
+        // left channel
         short Sample;
         mad_fixed_t Fixed = Synth.pcm.samples[0][FramePos];
-        if (Fixed >= MAD_F_ONE)
-          Sample = 0x7fff;
-        else if (Fixed <= -MAD_F_ONE)
-          Sample = -0x7fff;
-        else
-          Sample = Fixed >> (MAD_F_FRACBITS - 15);
-        Data[CurSample * 2] = Sample;
+             if (Fixed >= MAD_F_ONE) Sample = 0x7fff;
+        else if (Fixed <= -MAD_F_ONE) Sample = -0x7fff;
+        else Sample = Fixed>>(MAD_F_FRACBITS-15);
+        Data[CurSample*2] = Sample;
 
-        //  Right channel. If the decoded stream is monophonic then
-        // the right output channel is the same as the left one.
-        if (MAD_NCHANNELS(&Frame.header) == 2)
-        {
+        // right channel
+        // if the decoded stream is monophonic then the right output channel is the same as the left one
+        if (MAD_NCHANNELS(&Frame.header) == 2) {
           Fixed = Synth.pcm.samples[1][FramePos];
-          if (Fixed >= MAD_F_ONE)
-            Sample = 0x7fff;
-          else if (Fixed <= -MAD_F_ONE)
-            Sample = -0x7fff;
-          else
-            Sample = Fixed >> (MAD_F_FRACBITS - 15);
+               if (Fixed >= MAD_F_ONE) Sample = 0x7fff;
+          else if (Fixed <= -MAD_F_ONE) Sample = -0x7fff;
+          else Sample = Fixed>>(MAD_F_FRACBITS-15);
         }
-        Data[CurSample * 2 + 1] = Sample;
-        CurSample++;
-        //  Check if we already have decoded enough.
-        if (CurSample >= NumSamples)
-          return CurSample;
+        Data[CurSample*2+1] = Sample;
+
+        ++CurSample;
+        // check if we already have decoded enough
+        if (CurSample >= NumSamples) return CurSample;
       }
-      //  We are done with the frame.
+      // we are done with the frame
       HaveFrame = false;
     }
 
-    //  Fill in input buffer if it becomes empty.
-    if (Stream.buffer == nullptr || Stream.error == MAD_ERROR_BUFLEN)
-    {
-      if (!ReadData())
-        break;
+    // fill in input buffer if it becomes empty
+    if (Stream.buffer == nullptr || Stream.error == MAD_ERROR_BUFLEN) {
+      if (!ReadData()) break;
     }
 
-    //  Decode the next frame.
-    if (mad_frame_decode(&Frame, &Stream))
-    {
-      if (MAD_RECOVERABLE(Stream.error) ||
-        Stream.error==MAD_ERROR_BUFLEN)
-      {
-        continue;
-      }
-      else
-      {
-        break;
-      }
+    // decode the next frame
+    if (mad_frame_decode(&Frame, &Stream)) {
+      if (MAD_RECOVERABLE(Stream.error) || Stream.error == MAD_ERROR_BUFLEN) continue;
+      break;
     }
 
-    //  Once decoded the frame is synthesized to PCM samples.
+    // once decoded the frame is synthesized to PCM samples
     mad_synth_frame(&Synth, &Frame);
     FramePos = 0;
     HaveFrame = true;
-  } while(1);
+  }
   return CurSample;
 }
+
 
 //==========================================================================
 //
 //  VMp3AudioCodec::ReadData
 //
 //==========================================================================
-
-int VMp3AudioCodec::ReadData()
-{
-  int     ReadSize;
-  int     Remaining;
+int VMp3AudioCodec::ReadData () {
+  int ReadSize;
+  int Remaining;
   vuint8 *ReadStart;
 
-  //  If there are some bytes left, move them to the beginning of the
-  // buffer.
-  if (Stream.next_frame != nullptr)
-  {
-    Remaining = Stream.bufend - Stream.next_frame;
+  // if there are some bytes left, move them to the beginning of the buffer
+  if (Stream.next_frame != nullptr) {
+    Remaining = Stream.bufend-Stream.next_frame;
     memmove(InputBuffer, Stream.next_frame, Remaining);
-    ReadStart = InputBuffer + Remaining;
-    ReadSize = INPUT_BUFFER_SIZE - Remaining;
-  }
-  else
-  {
+    ReadStart = InputBuffer+Remaining;
+    ReadSize = INPUT_BUFFER_SIZE-Remaining;
+  } else {
     ReadSize = INPUT_BUFFER_SIZE;
     ReadStart = InputBuffer;
     Remaining = 0;
   }
-  //  Fill-in the buffer.
-  if (ReadSize > BytesLeft)
-    ReadSize = BytesLeft;
-  if (!ReadSize)
-    return 0;
+
+  // fill-in the buffer
+  if (ReadSize > BytesLeft) ReadSize = BytesLeft;
+  if (!ReadSize) return 0;
   Strm->Serialise(ReadStart, ReadSize);
   BytesLeft -= ReadSize;
 
-  //  When decoding the last frame of a file, it must be followed by
-  // MAD_BUFFER_GUARD zero bytes if one wants to decode that last frame.
-  if (!BytesLeft)
-  {
-    memset(ReadStart + ReadSize + Remaining, 0, MAD_BUFFER_GUARD);
+  // when decoding the last frame of a file, it must be followed by
+  // MAD_BUFFER_GUARD zero bytes if one wants to decode that last frame
+  if (!BytesLeft) {
+    memset(ReadStart+ReadSize+Remaining, 0, MAD_BUFFER_GUARD);
     ReadSize += MAD_BUFFER_GUARD;
   }
 
-  //  Pipe the new buffer content to libmad's stream decoder facility.
-  mad_stream_buffer(&Stream, InputBuffer, ReadSize + Remaining);
+  // pipe the new buffer content to libmad's stream decoder facility
+  mad_stream_buffer(&Stream, InputBuffer, ReadSize+Remaining);
   Stream.error = MAD_ERROR_NONE;
-  return ReadSize + Remaining;
+  return ReadSize+Remaining;
 }
+
 
 //==========================================================================
 //
 //  VMp3AudioCodec::Finished
 //
 //==========================================================================
-
-bool VMp3AudioCodec::Finished()
-{
-  //  We are done if there's no more data and last frame has been decoded.
-  return !BytesLeft && !HaveFrame;
+bool VMp3AudioCodec::Finished () {
+  // we are done if there's no more data and last frame has been decoded
+  return (!BytesLeft && !HaveFrame);
 }
+
 
 //==========================================================================
 //
 //  VMp3AudioCodec::Restart
 //
 //==========================================================================
-
-void VMp3AudioCodec::Restart()
-{
-  //  Seek to the beginning of the file.
-  Strm->Seek(0);
-  BytesLeft = Strm->TotalSize();
+void VMp3AudioCodec::Restart () {
+  // seek to the beginning of the data stream
+  Strm->Seek(StreamStart);
+  BytesLeft = StreamSize;
 }
+
 
 //==========================================================================
 //
 //  VMp3AudioCodec::Create
 //
 //==========================================================================
-
-VAudioCodec *VMp3AudioCodec::Create(VStream *InStrm)
-{
+VAudioCodec *VMp3AudioCodec::Create (VStream *InStrm) {
   VMp3AudioCodec *Codec = new VMp3AudioCodec(InStrm, true);
-  if (!Codec->Init())
-  {
+  if (!Codec->Init()) {
     delete Codec;
-    Codec = nullptr;
     return nullptr;
   }
   return Codec;
 }
+
 
 //==========================================================================
 //
 //  VMp3SampleLoader::Load
 //
 //==========================================================================
-
-void VMp3SampleLoader::Load(sfxinfo_t &Sfx, VStream &Stream)
-{
+void VMp3SampleLoader::Load (sfxinfo_t &Sfx, VStream &Stream) {
   VMp3AudioCodec *Codec = new VMp3AudioCodec(&Stream, false);
-  if (!Codec->Init()) {
-    //Codec->Cleanup();
-  } else {
+  if (Codec->Init()) {
     LoadFromAudioCodec(Sfx, Codec);
   }
   delete Codec;
