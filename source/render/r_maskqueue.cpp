28c045c4f0f7adda196b030dba6465bf1d4da6af
Ketmar Dark <ketmar@ketmar.no-ip.org>
2019-08-21 19:06:40 +0300

    engine: cvar to not fix sprite offsets for projectiles

--- source/render/r_maskqueue.cpp
+++ source/render/r_maskqueue.cpp
@@ -50,6 +50,7 @@ extern VCvarB r_brightmaps;
 extern VCvarB r_brightmaps_sprite;
 
 static VCvarB r_fix_sprite_offsets("r_fix_sprite_offsets", true, "Fix sprite offsets?", CVAR_Archive);
+static VCvarB r_fix_sprite_offsets_missiles("r_fix_sprite_offsets_missiles", false, "Fix sprite offsets for projectiles?", CVAR_Archive);
 static VCvarI r_sprite_fix_delta("r_sprite_fix_delta", "-7", "Sprite offset amount.", CVAR_Archive); // -6 seems to be ok for vanilla BFG explosion, and for imp fireball
 
 static VCvarB r_sprite_use_pofs("r_sprite_use_pofs", true, "Use PolygonOffset with sprite sorting to reduce sprite flickering?", CVAR_Archive);
@@ -459,7 +460,10 @@ void VRenderLevelShared::QueueSprite (VEntity *thing, vuint32 light, vuint32 Fad
   const int origTOffset = TexTOffset;
   //if (thing) GCon->Logf(NAME_Debug, "*** CLASS '%s': scaleY=%g; TOfs=%d; hgt=%d; dofix=%d", thing->GetClass()->GetName(), scaleY, TexTOffset, TexHeight, (TexTOffset < TexHeight && 2*TexTOffset+r_sprite_fix_delta >= TexHeight ? 1 : 0));
   //k8: scale is arbitrary here
-  if (r_fix_sprite_offsets && TexTOffset < TexHeight && 2*TexTOffset+r_sprite_fix_delta >= TexHeight && scaleY > 0.6f && scaleY < 1.6f) TexTOffset = TexHeight;
+  //    also, don't bother with projectiles, they're usually flying anyway
+  bool doFixSpriteOffset = r_fix_sprite_offsets;
+  if (doFixSpriteOffset && !r_fix_sprite_offsets_missiles && thing->IsMissile()) doFixSpriteOffset = false;
+  if (doFixSpriteOffset && TexTOffset < TexHeight && 2*TexTOffset+r_sprite_fix_delta >= TexHeight && scaleY > 0.6f && scaleY < 1.6f) TexTOffset = TexHeight;
 
   TVec topdelta = TexTOffset*sprup*scaleY;
   TVec botdelta = (TexTOffset-TexHeight)*sprup*scaleY;
