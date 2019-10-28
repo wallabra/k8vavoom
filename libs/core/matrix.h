4f42b5047d1f016b89b9e0e4438683c0dda0a672
Ketmar Dark <ketmar@ketmar.no-ip.org>
2019-10-28 09:08:47 +0200

    corelib: added `VViewPortMats()` helper class to matrix lib

--- libs/core/matrix.h
+++ libs/core/matrix.h
@@ -24,6 +24,7 @@
 //**
 //**************************************************************************
 
+// ////////////////////////////////////////////////////////////////////////// //
 class VMatrix4 {
 public:
   float m[4][4];
@@ -319,8 +320,11 @@ public:
   VMatrix4 &invert () noexcept;
 
   void toQuaternion (float quat[4]) const noexcept;
+
 };
 
+
+// ////////////////////////////////////////////////////////////////////////// //
 static inline __attribute__((unused)) TVec operator * (const VMatrix4 &mt, const TVec &v) noexcept {
   return TVec(
     VSUM4(mt.m[0][0]*v.x, mt.m[1][0]*v.y, mt.m[2][0]*v.z, mt.m[3][0]),
@@ -367,5 +371,52 @@ public:
       VSUM3(mt.m[1][0]*v.x, mt.m[1][1]*v.y, mt.m[1][2]*v.z),
       VSUM3(mt.m[2][0]*v.x, mt.m[2][1]*v.y, mt.m[2][2]*v.z)
     );
+  }
+};
+
+
+// ////////////////////////////////////////////////////////////////////////// //
+// view matrices
+class VViewPortMats {
+public:
+  // usually bottom-to-up
+  struct ViewPort {
+  public:
+    int x0, y0;
+    int width, height;
+    float scrwmid, scrhmid; // width/2, height/2
+  public:
+    // default is "not initialised"
+    ViewPort () noexcept /*: x0(0), y0(0), width(640), height(480), scrwmid(320), scrhmid(240)*/ {}
+    inline void setOrigin (int x, int y) noexcept { x0 = x; y0 = y; }
+    inline void setSize (int w, int h) noexcept { width = w; height = h; scrwmid = w*0.5f; scrhmid = h*0.5f; }
+    inline bool isValid () const noexcept { return (width > 0 && height > 0); }
+    inline int getX1 () const noexcept { return x0+width-1; }
+    inline int getY1 () const noexcept { return y0+height-1; }
+    inline void getX1Y1 (int *x1, int *y1) const noexcept { *x1 = x0+width-1; *y1 = y0+height-1; }
+  };
+
+public:
+  VMatrix4 projMat; // projection matrix, can be taken from OpenGL
+  VMatrix4 modelMat; // model->world transformation matrix, can be taken from OpenGL
+  ViewPort vport;
+
+public:
+  // default is "not initialised"
+  VViewPortMats () noexcept : projMat(), modelMat(), vport() {}
+
+  // transform model coords to world coords
+  inline TVec toWorld (const TVec &point) const noexcept { return modelMat*point; }
+
+  // transform world coords to viewport projected coords
+  // you can get world coords from model coords with `toWorld()`
+  // WARNING! `point` should not be behind near z clipping plane
+  inline void project (const TVec &point, int *scrx, int *scry) const noexcept {
+    TVec proj = projMat.Transform2OnlyXY(point); // we don't care about z here
+    const float pjw = -1.0f/point.z;
+    proj.x *= pjw;
+    proj.y *= pjw;
+    *scrx = vport.x0+(int)((1.0f+proj.x)*vport.scrwmid);
+    *scry = vport.y0+(int)((1.0f+proj.y)*vport.scrhmid);
   }
 };
