23d3813f869b3719017ad110339f4ad188b2dfa4
Ketmar Dark <ketmar@ketmar.no-ip.org>
2020-04-23 19:35:39 +0300

    engine: sound: added some log messages to OPL player

--- source/sound/snd_nuked.cpp
+++ source/sound/snd_nuked.cpp
@@ -75,13 +75,17 @@ void VNukedOPLAudioCodec::loadGenMIDI () {
   if (gmlumpTried) return;
   gmlumpTried = -1;
   int lump = W_CheckNumForName("genmidi");
-  if (lump < 0) return;
+  if (lump < 0) {
+    GCon->Log(NAME_Warning, "\"genmidi\" lump not found, OPL synthesis disabled.");
+    return;
+  }
   VStream *strm = W_CreateLumpReaderNum(lump);
   if (!strm) return;
   int size = strm->TotalSize();
   if (size < 8 || size > 65536 || strm->IsError()) {
     strm->Close();
     delete strm;
+    GCon->Log(NAME_Warning, "\"genmidi\" has invalid size, OPL synthesis disabled.");
     return;
   }
   genmidi.setLength(size);
@@ -89,7 +93,13 @@ void VNukedOPLAudioCodec::loadGenMIDI () {
   bool err = strm->IsError();
   strm->Close();
   delete strm;
-  if (err) { genmidi.clear(); return; }
+  if (err) {
+    genmidi.clear();
+    GCon->Log(NAME_Warning, "cannot load \"genmidi\", OPL synthesis disabled.");
+    return;
+  }
+
+  GCon->Logf("loaded \"genmidi\" from '%s'", *W_FullLumpName(lump));
   gmlumpTried = 1;
 }
 
@@ -109,6 +119,7 @@ VNukedOPLAudioCodec::VNukedOPLAudioCodec (VStream *InStrm)
   musplr.sendConfig(48000, (snd_nukedopl_type.asInt() > 0), snd_nukedopl_stereo.asBool());
   if (!musplr.isGenMIDILoaded()) {
     if (!musplr.loadGenMIDI(genmidi.ptr(), (size_t)genmidi.length())) {
+      GCon->Log(NAME_Warning, "cannot parse \"genmidi\" lump, OPL synthesis disabled.");
       gmlumpTried = -1; // do not try it anymore
       return;
     }
