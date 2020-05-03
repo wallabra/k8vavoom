4300b3f9911ddb9e4f3a9b4e200a828016643327
Ketmar Dark <ketmar@ketmar.no-ip.org>
2020-05-03 04:03:18 +0300

    engine: script: changed default format for player color, and fixed color sliders

--- source/cl_main.cpp
+++ source/cl_main.cpp
@@ -49,7 +49,7 @@ VClientGameBase *GClGame = nullptr;
 bool UserInfoSent = false;
 
 VCvarS cl_name("name", "PLAYER", "Player name.", CVAR_Archive|CVAR_UserInfo);
-VCvarS cl_color("color", "00 ff 00", "Player color.", CVAR_Archive|CVAR_UserInfo);
+VCvarS cl_color("color", "#00ff00", "Player color.", CVAR_Archive|CVAR_UserInfo);
 VCvarI cl_class("class", "0", "Player class.", /*CVAR_Archive|*/CVAR_UserInfo); // do not save it, because it may interfere with other games
 VCvarS cl_model("model", "", "Player model.", CVAR_Archive|CVAR_UserInfo);
 
