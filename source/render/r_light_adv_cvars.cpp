688c7db7caff8cfd952acf6e30b3ce492b4543d9
Ketmar Dark <ketmar@ketmar.no-ip.org>
2020-12-01 22:51:10 +0200

    render: turned off collector by default (it is buggy, at least with shadowmaps)

--- source/render/r_light_adv_cvars.cpp
+++ source/render/r_light_adv_cvars.cpp
@@ -43,4 +43,5 @@ VCvarB dbg_adv_light_notrace_mark("dbg_adv_light_notrace_mark", false, "Mark not
 VCvarB r_advlight_opt_optimise_scissor("r_advlight_opt_optimise_scissor", true, "Optimise scissor with lit geometry bounds.", CVAR_Archive);
 
 
-VCvarB r_adv_use_collector("r_adv_use_collector", true, "Use one collect pass for advanced lighting?", CVAR_Archive);
+// it's buggy yet
+VCvarB r_adv_use_collector("r_adv_use_collector", false, "Use one collect pass for advanced lighting?", CVAR_Archive);
