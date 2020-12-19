6f0c344a0c25e0112bb698b10596976af4a32367
Ketmar Dark <ketmar@ketmar.no-ip.org>
2020-12-19 12:49:07 +0200

    render: made "r_shadowmap_fix_light_dist" false by default: it causes light jutter, and current shadowmap checker shouldn't need it

--- source/render/r_light_adv.cpp
+++ source/render/r_light_adv.cpp
@@ -39,7 +39,7 @@ static VCvarB r_shadowvol_use_pofs("r_shadowvol_use_pofs", true, "Use PolygonOff
 static VCvarF r_shadowvol_pofs("r_shadowvol_pofs", "20", "DEBUG");
 static VCvarF r_shadowvol_pslope("r_shadowvol_pslope", "-0.2", "DEBUG");
 
-static VCvarB r_shadowmap_fix_light_dist("r_shadowmap_fix_light_dist", true, "Move lights slightly away from surfaces?", /*CVAR_PreInit|*/CVAR_Archive);
+static VCvarB r_shadowmap_fix_light_dist("r_shadowmap_fix_light_dist", false, "Move lights slightly away from surfaces?", /*CVAR_PreInit|*/CVAR_Archive);
 VCvarI r_shadowmap_sprshadows("r_shadowmap_sprshadows", "2", "Render shadows from sprites (0:none;1:non-rotational;2:all)?", /*CVAR_PreInit|*/CVAR_Archive);
 VCvarB r_shadowmap_sprshadows_player("r_shadowmap_sprshadows_player", true, "Render player sprite shadow?", /*CVAR_PreInit|*/CVAR_Archive);
 
